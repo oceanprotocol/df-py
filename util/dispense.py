cb059c97fbf7668818aa0d225289f77fd8c89618
trentmc <5305452+trentmc@users.noreply.github.com>
2022-03-28 08:16:06 +0200

    add stubs for dispense

---
+++ util/dispense.py
@@ -0,0 +1,1 @@
+import brownie
---
+++ util/test/test_dispense.py
@@ -0,0 +1,10 @@
+from util import oceanutil
+
+def test_1():
+    #create testing file
+
+    #measure OCEAN balances before
+
+    #call dispense, based on test file
+
+    #check OCEAN balances
