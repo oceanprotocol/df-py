9bf796fbe99c67819b265af5028749581c0560e5
trentmc <5305452+trentmc@users.noreply.github.com>
2022-03-28 08:57:06 +0200

    building dispense: test is built, failing on purpose

--- util/dispense.py
+++ util/dispense.py
@@ -1,1 +1,54 @@
-import brownie
+import csv
+import os
+
+def dispenseRewards(csv_dir:str):
+    """@arguments -- csv_dir -- directory path for csv file"""
+    pass #FIXME
+
+def rewardsPathToFile(path:str) -> str:
+    return os.path.join(path, 'rewards.csv')
+
+def rewardsToCsv(rewards:dict, csv_dir:str) -> str:
+    """
+    @description
+      Given rewards dict, store is as csv:
+
+      address  OCEAN_reward  
+      0x123    123.123
+      0x456    456.456
+      ..       ..
+
+    @arguments
+      rewards -- dict of [LP_addr] : OCEAN_float
+      csv_dir -- directory path for csv file
+    """
+    csv_file = rewardsPathToFile(csv_dir)
+    assert not os.path.exists(csv_file), f"{csv_file} can't already exist"
+    with open(csv_file, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(["address", "OCEAN_reward"])
+        for address, OCEAN_reward in rewards.items():
+            writer.writerow([address, OCEAN_reward])
+    print(f"Filled rewards file: {csv_file}")
+
+def csvToRewards(dir):
+    """
+    @description
+      Given rewards csv, extract it as dict
+
+    @arguments
+      csv_dir -- directory path for csv file
+
+    @return
+      rewards -- dict of [LP_addr] : OCEAN_float
+    """
+    csv_file = rewardsPathToFile(csv_dir)
+    rewards = {}
+    with open(csv_file, 'r') as f:
+        reader = csv.writer(f)
+        for row_i, row in enumerate(reader):
+            if row_i > 0:
+                (address, OCEAN_reward) = row
+                rewards[address] = OCEAN_reward
+    return rewards
+
--- util/test/test_dispense.py
+++ util/test/test_dispense.py
@@ -1,10 +1,36 @@
+import brownie
+import os
+
+from util import dispense
 from util import oceanutil
+from util.base18 import fromBase18
 
-def test_1():
-    #create testing file
+def test_1(ADDRESS_FILE):
+    #set accounts
+    oceanutil.recordDeployedContracts(ADDRESS_FILE, "development")
+    test_accounts = [brownie.network.accounts[i].address for i in range(5, 10)]
+    OCEAN = oceanutil.OCEANtoken()
+    balances_before = {account : fromBase18(OCEAN.balanceOf(account))
+                       for account in test_accounts}
 
-    #measure OCEAN balances before
+    #set fake rewards
+    rewards = {account : account_i * 100.0 
+               for account_i, account in enumerate(test_accounts)}
 
-    #call dispense, based on test file
+    #set test file
+    csv_dir = '/tmp'
+    csv_file = dispense.rewardsPathToFile(csv_dir)
+    if os.path.exists(csv_file):
+        os.remove(csv_file)
+    dispense.rewardsToCsv(rewards, csv_dir)
 
-    #check OCEAN balances
+    #**MAIN WORK: call dispense**
+    dispense.dispenseRewards(csv_dir)
+
+    #did dispense work?
+    for account in test_accounts:
+        bal_before = balances_before[account]
+        bal_after = fromBase18(OCEAN.balanceOf(account))
+        exp_reward = rewards[account]
+        assert (bal_before + exp_reward) == bal_after
+                               
