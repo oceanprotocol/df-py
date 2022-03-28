81f9a270d725901040f7c21f4dc34241049f16c7
trentmc <5305452+trentmc@users.noreply.github.com>
2022-03-28 17:47:51 +0200

    better document

--- sol0.6.2/contracts/MerkleAirdrop.sol
+++ sol0.6.2/contracts/MerkleAirdrop.sol
@@ -1,4 +1,6 @@
 // SPDX-License-Identifier: (Apache-2.0 AND CC-BY-4.0)
+// Contract is lightly adapted from https://medium.com/mochilab/merkle-airdrop-one-of-the-best-airdrop-solution-for-token-issues-e2279df1c5c1
+
 
 pragma solidity ^0.6.0;
 pragma experimental ABIEncoderV2;
@@ -8,19 +10,11 @@ import "OpenZeppelin/openzeppelin-contracts@3.0.1/contracts/token/ERC20/SafeERC2
 import "OpenZeppelin/openzeppelin-contracts@3.0.1/contracts/math/SafeMath.sol";
 import "OpenZeppelin/openzeppelin-contracts@3.0.1/contracts/access/Ownable.sol";
 
-/*
-Contracts is from https://medium.com/mochilab/merkle-airdrop-one-of-the-best-airdrop-solution-for-token-issues-e2279df1c5c1
-
-
-- Finally, the _disburse function is the function that will send the token from the contract to the user when all conditions are satisfied.
-
-*/
-
 contract MerkleAirdrop is Ownable {
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
  
-    event Claimed(address claimant, uint256 week, uint256 balance);
+    event Claimed(address claimant, uint256 tranche, uint256 balance);
     event TrancheAdded(uint256 tranche, bytes32 merkleRoot, uint256 totalAmount);
     event TrancheExpired(uint256 tranche);
     event RemovedFunder(address indexed _address);
@@ -34,14 +28,14 @@ contract MerkleAirdrop is Ownable {
     // [tranche][address] : claimed
     mapping(uint256 => mapping(address => bool)) public claimed;
 
-    // tranche id
+    // current tranche id. Increments with each new allocation of rewards
     uint256 public tranches;
     
     constructor(IERC20 _token) public {
 	token = _token;
     }
 
-    // OPF calls this after new weekly rewards are calculated
+    // OPF calls this to initialize a new tranche (airdrop round).
     function seedNewAllocations(bytes32 _merkleRoot, uint256 _totalAllocation)
 	public onlyOwner
 	returns (uint256 trancheId)
@@ -53,7 +47,7 @@ contract MerkleAirdrop is Ownable {
 	emit TrancheAdded(trancheId, _merkleRoot, _totalAllocation);
     }
 
-    // OPF calls this to retire a given week (tranche
+    // OPF calls this to retire a given tranche, therefore no longer claimable
     function expireTranche(uint256 _trancheId)
 	public onlyOwner
     {
@@ -61,29 +55,31 @@ contract MerkleAirdrop is Ownable {
 	emit TrancheExpired(_trancheId);
     }
 
-    // An LP can call this to get OCEAN for a given week (tranche)
-    function claimWeek(address _liquidityProvider,
-		       uint256 _tranche,
-		       uint256 _balance,
-		       bytes32[] memory _merkleProof)
+    // An LP can call this to get OCEAN for a given tranche (e.g. week)
+    function claimTranche(address _liquidityProvider,
+			  uint256 _tranche,
+			  uint256 _balance,
+			  bytes32[] memory _merkleProof)
 	public
     {
-	_claimWeek(_liquidityProvider, _tranche, _balance, _merkleProof);
+	_claimTranche(_liquidityProvider, _tranche, _balance, _merkleProof);
 	_disburse(_liquidityProvider, _balance);
     }
     
-    // An LP can call this to get OCEAN for >=1 weeks (tranches)
-    function claimWeeks(address _liquidityProvider,
-			uint256[] memory _tranches,
-			uint256[] memory _balances,
-			bytes32[][] memory _merkleProofs)
+    // An LP can call this to get OCEAN for >=1 tranches (eg weeks)
+    function claimTranches(address _liquidityProvider,
+			   uint256[] memory _tranches,
+			   uint256[] memory _balances,
+			   bytes32[][] memory _merkleProofs)
 	public
     {
 	uint256 len = _tranches.length;
-	require(len == _balances.length && len == _merkleProofs.length, "Mismatching inputs");
+	require(len == _balances.length && len == _merkleProofs.length,
+		"Mismatching inputs");
 	uint256 totalBalance = 0;
 	for(uint256 i = 0; i < len; i++) {
-	    _claimWeek(_liquidityProvider, _tranches[i], _balances[i], _merkleProofs[i]);
+	    _claimTranche(_liquidityProvider, _tranches[i], _balances[i],
+			  _merkleProofs[i]);
 	    totalBalance = totalBalance.add(_balances[i]);
 	}
 	_disburse(_liquidityProvider, totalBalance);
@@ -100,16 +96,17 @@ contract MerkleAirdrop is Ownable {
 	return _verifyClaim(_liquidityProvider, _tranche, _balance, _merkleProof);
     }
 
-    // Private function - support for claimWeek() and claimWeeks()
-    function _claimWeek(address _liquidityProvider,
+    // Private function - support for claimTranche() and claimTranches()
+    function _claimTranche(address _liquidityProvider,
 			uint256 _tranche,
 			uint256 _balance,
 			bytes32[] memory _merkleProof)	
 	private
     {
-	require(_tranche < tranches, "Week cannot be in the future");
+	require(_tranche < tranches, "Tranche cannot be in the future");
 	require(!claimed[_tranche][_liquidityProvider], "LP has already claimed");
-	require(_verifyClaim(_liquidityProvider, _tranche, _balance, _merkleProof), "Incorrect merkle proof");
+	require(_verifyClaim(_liquidityProvider, _tranche, _balance, _merkleProof),
+		"Incorrect merkle proof");
 	claimed[_tranche][_liquidityProvider] = true;
 	emit Claimed(_liquidityProvider, _tranche, _balance);
     }
