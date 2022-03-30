4dbb35c5532698963073feabbe81d625c2044889
trentmc <5305452+trentmc@users.noreply.github.com>
2022-03-30 11:58:49 +0200

    test OCEAN airdrop

--- util/test/test_Airdrop.py
+++ util/test/test_Airdrop.py
@@ -2,24 +2,25 @@ import brownie
 
 from util.constants import BROWNIE_PROJECT as B
 from util.base18 import toBase18, fromBase18
+from util import oceanutil
 
 accounts = brownie.network.accounts
 a1, a2, a3 = accounts[1].address, accounts[2].address, accounts[3].address
 
 def test_no_allocate():
-    TOK = _deployToken(accounts[0])
+    TOK = _deployTOK(accounts[0])
     airdrop = B.Airdrop.deploy(TOK.address, {"from": accounts[0]})
     assert airdrop.claimable(a1) == 0
 
-def test_allocate():
-    TOK = _deployToken(accounts[9])
+def test_TOK():
+    TOK = _deployTOK(accounts[9])
     TOK.transfer(accounts[0].address, toBase18(100.0), {"from": accounts[9]})
     
     airdrop = B.Airdrop.deploy(TOK.address, {"from": accounts[0]})
         
     tos = [a1, a2, a3]
     values = [10, 20, 30]
-    TOK.approve(airdrop, 10+20+30, {"from": accounts[0]})
+    TOK.approve(airdrop, sum(values), {"from": accounts[0]})
     airdrop.allocate(tos, values, {"from": accounts[0]})
     
     assert airdrop.claimable(a1) == 10
@@ -41,7 +42,24 @@ def test_allocate():
     airdrop.claimFor(a3, {"from": accounts[9]})
     assert TOK.balanceOf(a3) == 30
 
-def _deployToken(account):
+def test_OCEAN(ADDRESS_FILE):
+    oceanutil.recordDeployedContracts(ADDRESS_FILE, "development")
+    OCEAN = oceanutil.OCEANtoken()
+    assert OCEAN.balanceOf(accounts[0]) >= 10
+    
+    airdrop = B.Airdrop.deploy(OCEAN.address, {"from": accounts[0]})
+        
+    OCEAN.approve(airdrop, 10, {"from": accounts[0]})
+    airdrop.allocate([a1], [10], {"from": accounts[0]})
+    
+    assert airdrop.claimable(a1) == 10
+
+    bal_before = OCEAN.balanceOf(a1)
+    airdrop.claim({"from": accounts[1]})
+    bal_after = OCEAN.balanceOf(a1)
+    assert (bal_after - bal_before) == 10    
+
+def _deployTOK(account):
     return B.Simpletoken.deploy(
         "TOK", "TOK", 18, toBase18(100.0), {"from": account})
     
