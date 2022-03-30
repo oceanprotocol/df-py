646b6e687446afe942051d11a37326477db7feb0
trentmc <5305452+trentmc@users.noreply.github.com>
2022-03-30 13:25:20 +0200

    batching. all tests pass

--- util/dispense.py
+++ util/dispense.py
@@ -4,7 +4,9 @@ import os
 from util.constants import BROWNIE_PROJECT as B
 from util.base18 import fromBase18, toBase18
 
-def dispenseRewards(csv_dir:str, airdrop_addr:str, from_account):
+BATCH_SIZE = 100
+
+def dispenseFromCsv(csv_dir:str, airdrop_addr:str, from_account):
     """
     @description
       Sends tokens to addresses, as specified in CSV file. 
@@ -16,10 +18,29 @@ def dispenseRewards(csv_dir:str, airdrop_addr:str, from_account):
       from_account --
     """
     [tos, _, values_int] = csvToRewardsLists(csv_dir)
+    dispenseFromLists(tos, values_int, airdrop_addr, from_account)
+    
+def dispenseFromLists(
+        tos: list, values_int:list, airdrop_addr:str, from_account,
+        batch_size:int=BATCH_SIZE):
+    assert len(tos) == len(values_int)
+    
+    print("dispense: begin")
+    print(f"  # addresses: {len(tos)}")
+    
     airdrop = B.Airdrop.at(airdrop_addr)
     TOK = B.Simpletoken.at(airdrop.getToken())
+    print(f"  Total amount: {fromBase18(sum(values_int))} {TOK.symbol()}")
+    
     TOK.approve(airdrop, sum(values_int), {"from": from_account})
-    airdrop.allocate(tos, values_int, {"from": from_account})
+
+    N = len(tos)
+    sts = list(range(N))[::batch_size] #send in batches to avoid gas issues
+    for i, st in enumerate(sts):
+        fin = st + batch_size
+        print(f"  Batch #{(i+1)}/{len(sts)}, {len(tos[st:fin])} addresses")
+        airdrop.allocate(tos[st:fin], values_int[st:fin], {"from":from_account})
+    print("dispense: done")
     
 def rewardsPathToFile(path:str) -> str:
     return os.path.join(path, 'rewards.csv')
--- util/test/test_dispense.py
+++ util/test/test_dispense.py
@@ -18,16 +18,16 @@ def test_csv():
     assert rewards == {to:value_f for to,value_f in zip(tos, values_float)} 
     assert values_float == [fromBase18(value_int) for value_int in values_int]
 
-def test_main(ADDRESS_FILE):
+def test_dispenseFromCsv(ADDRESS_FILE):
     recordDeployedContracts(ADDRESS_FILE, "development")
     OCEAN = OCEANtoken()
 
-    rewards = {a1:0.1, a2:0.2, a3:0.3}
-    _rewardsToCsvWithRemove(rewards, '/tmp')    
-
     airdrop = B.Airdrop.deploy(OCEAN.address, {"from": accounts[0]})
     
-    dispense.dispenseRewards('/tmp', airdrop.address, accounts[0])
+    rewards = {a1:0.1, a2:0.2, a3:0.3}
+    _rewardsToCsvWithRemove(rewards, '/tmp')
+
+    dispense.dispenseFromCsv('/tmp', airdrop.address, accounts[0])
 
     #a1 claims for itself
     bal_before = fromBase18(OCEAN.balanceOf(a1))
@@ -40,6 +40,21 @@ def test_main(ADDRESS_FILE):
     airdrop.claimFor(a3, {"from": accounts[9]})
     bal_after = fromBase18(OCEAN.balanceOf(a3))
     assert (bal_after - bal_before) == pytest.approx(0.3)
+
+def test_dispenseFromLists_with_batching(ADDRESS_FILE):
+    recordDeployedContracts(ADDRESS_FILE, "development")
+    OCEAN = OCEANtoken()
+    
+    airdrop = B.Airdrop.deploy(OCEAN.address, {"from": accounts[0]})
+
+    batch_size = 3
+    N = batch_size * 3 + 1 #enough accounts to ensure batching
+    assert len(accounts) >= N
+    tos = [accounts[i] for i in range(N)]
+    values_int = [toBase18(i+1.0) for i in range(N)]
+    
+    dispense.dispenseFromLists(
+        tos, values_int, airdrop.address, accounts[0], batch_size=batch_size)
 
 def _rewardsToCsvWithRemove(rewards, csv_dir):
     csv_file = dispense.rewardsPathToFile(csv_dir)
