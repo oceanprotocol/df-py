c324382a161ca5806d628a4331383020728770dc
trentmc <5305452+trentmc@users.noreply.github.com>
2022-03-30 12:50:06 +0200

    dispenseRewards() is running end-to-end

--- util/dispense.py
+++ util/dispense.py
@@ -1,13 +1,25 @@
 import csv
 import os
 
-from util import oceanutil
 from util.constants import BROWNIE_PROJECT as B
 from util.base18 import fromBase18, toBase18
 
-def dispenseRewards(csv_dir:str, airdrop_contract_addr:str, from_account):
-    """@arguments -- csv_dir -- directory path for csv file"""
-    raise NotImplementedError()
+def dispenseRewards(csv_dir:str, airdrop_addr:str, from_account):
+    """
+    @description
+      Sends tokens to addresses, as specified in CSV file. 
+      Which token is specified in airdrop contract. (Not specific to OCEAN.)
+
+    @arguments
+      csv_dir -- str -- directory path for csv file
+      airdrop_addr -- str -- address of airdrop contract
+      from_account --
+    """
+    [tos, _, values_int] = csvToRewardsLists(csv_dir)
+    airdrop = B.Airdrop.at(airdrop_addr)
+    TOK = B.Simpletoken.at(airdrop.getToken())
+    TOK.approve(airdrop, sum(values_int), {"from": from_account})
+    airdrop.allocate(tos, values_int, {"from": from_account})
     
 def rewardsPathToFile(path:str) -> str:
     return os.path.join(path, 'rewards.csv')
@@ -17,43 +29,49 @@ def rewardsToCsv(rewards:dict, csv_dir:str) -> str:
     @description
       Given rewards dict, store as csv:
 
-      address  amt_OCEAN
+      address  value
       0x123    123.123
       0x456    456.456
       ..       ..
 
     @arguments
-      rewards -- dict of [LP_addr] : OCEAN_float
+      rewards -- dict of [to_addr] : value_float (*not* base 18)
       csv_dir -- directory path for csv file
     """
     csv_file = rewardsPathToFile(csv_dir)
     assert not os.path.exists(csv_file), f"{csv_file} can't already exist"
     with open(csv_file, 'w') as f:
         writer = csv.writer(f)
-        writer.writerow(["address", "amt_OCEAN"])
-        for address, OCEAN_reward in rewards.items():
-            writer.writerow([address, OCEAN_reward])
+        writer.writerow(["address", "value"])
+        for to_addr, value in rewards.items():
+            writer.writerow([to_addr, value])
     print(f"Filled rewards file: {csv_file}")
 
-def csvToRewards(csv_dir):
+def csvToRewardsLists(csv_dir):
     """
     @description
-      Given rewards csv, extract it as dict
+      Given rewards csv, extract it two lists
 
     @arguments
       csv_dir -- directory path for csv file
 
     @return
-      rewards -- dict of [address_str] : amt_OCEAN_float
+      tos -- list of to_addr_str
+      values_float -- list of value_float (*not* base 18)
+      values_int -- list of value_int (base 18, like wei)
     """
     csv_file = rewardsPathToFile(csv_dir)
-    rewards = {}
+    tos, values_float, values_int = [], [], []
     with open(csv_file, 'r') as f:
         reader = csv.reader(f)
         for row_i, row in enumerate(reader):
-            if row_i > 0:
-                address = row[0]
-                OCEAN_reward = float(row[1])
-                rewards[address] = OCEAN_reward
-    return rewards
+            if row_i == 0: #header
+                pass
+            else:
+                to, value_float = row[0], float(row[1])
+                value_int = toBase18(value_float)
+                tos.append(to)
+                values_float.append(value_float)
+                values_int.append(value_int)
+    return (tos, values_float, values_int)
 
--- util/test/test_dispense.py
+++ util/test/test_dispense.py
@@ -2,7 +2,8 @@ import brownie
 import os
 import pytest
 
-from util import dispense, oceanutil
+from util import dispense
+from util.oceanutil import recordDeployedContracts, OCEANtoken
 from util.base18 import fromBase18, toBase18
 from util.constants import BROWNIE_PROJECT as B
 
@@ -11,34 +12,24 @@ a1, a2, a3 = accounts[1].address, accounts[2].address, accounts[3].address
 
 def test_csv():
     rewards = {a1:0.1, a2:0.2, a3:0.3}
-    
-    csv_dir = '/tmp'
-    csv_file = dispense.rewardsPathToFile(csv_dir)
-    if os.path.exists(csv_file):
-        os.remove(csv_file)
-        
-    dispense.rewardsToCsv(rewards, csv_dir)
+    _rewardsToCsvWithRemove(rewards, '/tmp')
+    (tos, values_float, values_int) = dispense.csvToRewardsLists('/tmp')
+    assert len(tos) == len(values_float) == len(values_int)
+    assert rewards == {to:value_f for to,value_f in zip(tos, values_float)} 
+    assert values_float == [fromBase18(value_int) for value_int in values_int]
 
-    rewards2 = dispense.csvToRewards(csv_dir)
-
-    assert rewards == rewards2
-
-def test_1(ADDRESS_FILE):
-    oceanutil.recordDeployedContracts(ADDRESS_FILE, "development")
-    OCEAN = oceanutil.OCEANtoken()
+def test_via_direct(ADDRESS_FILE):
+    recordDeployedContracts(ADDRESS_FILE, "development")
+    OCEAN = OCEANtoken()
     airdrop = B.Airdrop.deploy(OCEAN.address, {"from": accounts[0]})
 
     rewards = {a1:0.1, a2:0.2, a3:0.3}
     
     tos = list(rewards.keys())
-    values = [toBase18(rewards[to]) for to in tos]
+    values_int = [toBase18(rewards[to]) for to in tos]
     
-    OCEAN.approve(airdrop, sum(values), {"from": accounts[0]})
-    airdrop.allocate(tos, values, {"from": accounts[0]})
-    
-    assert fromBase18(airdrop.claimable(a1)) == 0.1
-    assert fromBase18(airdrop.claimable(a2)) == 0.2
-    assert fromBase18(airdrop.claimable(a3)) == 0.3
+    OCEAN.approve(airdrop, sum(values_int), {"from": accounts[0]})
+    airdrop.allocate(tos, values_int, {"from": accounts[0]})
 
     #a1 claims for itself
     bal_before = fromBase18(OCEAN.balanceOf(a1))
@@ -52,3 +43,33 @@ def test_1(ADDRESS_FILE):
     bal_after = fromBase18(OCEAN.balanceOf(a3))
     assert (bal_after - bal_before) == pytest.approx(0.3)
 
+
+def test_via_dispenseRewards(ADDRESS_FILE):
+    recordDeployedContracts(ADDRESS_FILE, "development")
+    OCEAN = OCEANtoken()
+
+    rewards = {a1:0.1, a2:0.2, a3:0.3}
+    _rewardsToCsvWithRemove(rewards, '/tmp')    
+
+    airdrop = B.Airdrop.deploy(OCEAN.address, {"from": accounts[0]})
+    
+    dispense.dispenseRewards('/tmp', airdrop.address, accounts[0])
+
+    #a1 claims for itself
+    bal_before = fromBase18(OCEAN.balanceOf(a1))
+    airdrop.claim({"from": accounts[1]})
+    bal_after = fromBase18(OCEAN.balanceOf(a1))
+    assert (bal_after - bal_before) == pytest.approx(0.1)
+
+    #a9 claims on behalf of a1
+    bal_before = fromBase18(OCEAN.balanceOf(a3))
+    airdrop.claimFor(a3, {"from": accounts[9]})
+    bal_after = fromBase18(OCEAN.balanceOf(a3))
+    assert (bal_after - bal_before) == pytest.approx(0.3)
+
+def _rewardsToCsvWithRemove(rewards, csv_dir):
+    csv_file = dispense.rewardsPathToFile(csv_dir)
+    if os.path.exists(csv_file):
+        os.remove(csv_file)
+    dispense.rewardsToCsv(rewards, csv_dir)
+    return csv_file
