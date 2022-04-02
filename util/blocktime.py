60ad8534e8f2abc21f61434ea77813ba0ad4837a
trentmc <5305452+trentmc@users.noreply.github.com>
2022-04-02 06:43:06 +0200

    Towards #24 -- dftool: allow specify ST, FIN blocks with date/time

--- util/blocktime.py
+++ util/blocktime.py
@@ -1,22 +1,37 @@
+from datetime import datetime
+from enforce_typing import enforce_types
 
-def datetimeToBlock(chain, datetime):
+@enforce_types
+def timestrToBlock(chain, timestr:str) -> int:
     """
+    Examples: 2022-03-29 17:55 --> 4928
+              2022-03-29 --> 4928 (earliest block of the day)
+
     @arguments
       chain -- brownie.networks.chain
-      datetime -- YYYY:MM:DD | YYYY:MM:DD:HH:MM
+      timestr -- str - YYYY-MM-DD | YYYY-MM-DD HH:MM
     @return
       block -- int
     """
-    
-    timestamp = datetimeToTimestamp(datetime) #timestamp = unix time
+    timestamp = timestrToTimestamp(timestr)
     block = timestampToBlock(chain, timestamp)
     return block
 
-def datetimeToTimestamp(datetime):
-    raise NotImplementedError('build me')
-    #see https://note.nkmk.me/en/python-unix-time-datetime/
+@enforce_types
+def timestrToTimestamp(timestr:str) -> float:
+    """Examples: 2022-03-29 17:55 --> 1648872899.3 (unix time)
+                 2022-03-29 --> 1648872899.0
+    """
+    ncolon = timestr.count(":")
+    if ncolon == 1:
+        d = datetime.strptime(timestr, "%Y-%m-%d %H:%M")
+    else:
+        d = datetime.strptime(timestr, "%Y-%m-%d")
+    return d.timestamp()
 
-def timestampToBlock(chain, timestamp):    
+@enforce_types
+def timestampToBlock(chain, timestamp:float) -> int:
+    """Example: 1648872899.0 --> 4928"""
     raise NotImplementedError('build me')
 
     #1. get block 0 timestamp, block N timestamp, then bisect-search
--- util/test/test_blocktime.py
+++ util/test/test_blocktime.py
@@ -1,3 +1,14 @@
-def test1():
+from util.blocktime import timestrToBlock, timestrToTimestamp, \
+    timestampToBlock
+
+def test_timestrToBlock():
+    pass
+
+def test_timestrToTimestamp():
+    assert timestrToTimestamp("1970-01-01 1:00") == 0.0
+    assert timestrToTimestamp("2022-03-29 17:55") == 1648569300.0
+    assert timestrToTimestamp("2022-03-29") == 1648504800.0
+
+def test_timestampToBlock():
     pass
 
