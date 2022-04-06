15c1961501d50a88dfa640b8b8375031343e74bc
trentmc <5305452+trentmc@users.noreply.github.com>
2022-04-06 12:56:46 +0200

    compute exchange rates, using coingecko. All tests passing

--- util/getrate.py
+++ util/getrate.py
@@ -1,18 +1,77 @@
+from datetime import datetime, timedelta
 from enforce_typing import enforce_types
+import numpy
+from pycoingecko import CoinGeckoAPI
 
-def getrate(token_symbol:str, st:str, fin:str, nsamp:int, seed:int) -> float:
+def getrate(token_symbol:str, st:str, fin:str) -> float:
     """
     @description
-      Get the exchange rate for a token, averaged over a time period
+      Get the exchange rate for a token.
+      Takes one measure per day. Averages across each day. 
+      Max 40 days, since Coingecko API rate-limits to 50 calls/min.
 
     @arguments
       token_symbol -- str -- e.g. "OCEAN" or "H2O"
-      st -- str -- start time, in format YYYY-MM-DD | YYYY-MM-DD_HH:MM
-      fin -- str -- end time, in format YYYY-MM-DD | YYYY-MM-DD_HH:MM | now
-      nsamp -- int -- # times to randomly sample from in the time rnage
-      seed -- int -- random seed 
+      st -- start date in format "YYYY-MM-DD"
+      fin -- end date in format "YYYY-MM-DD"
     
     @return
-      rates -- float -- USD_per_token
+      rate -- float -- USD_per_token
     """
-    raise NotImplementedError()
+    #corner case
+    if token_symbol.lower() == "h2o":
+        return 1.618 #target peg. Update this when H2O is on coingecko
+    
+    st_dt = datetime.strptime(st, "%Y-%m-%d")
+    fin_dt = datetime.strptime(fin, "%Y-%m-%d")
+    num_days = (fin_dt - st_dt).days
+    if num_days < 0:
+        raise ValueError("Start date is after end date")
+    if num_days > 40:
+        raise ValueError("max 40 days, since coingecko rate-limits")
+
+    rates = []
+    for day_i in range(num_days+1):
+        dt = st_dt + timedelta(days=day_i)
+        timestr = f"{dt.year:04d}-{dt.month:02d}-{dt.day:02d}"
+        rate_day = coingeckoRate(token_symbol, timestr)
+        rates.append(rate_day)
+
+    rate = numpy.average(rates)
+    return rate
+          
+def coingeckoRate(token_symbol:str, timestr:str) -> float:
+    """
+    @arguments
+      token_symbol -- e.g. "OCEAN", "BTC"
+      timestr -- str in format "YYYY-MM-DD"
+    @return
+      rate -- float -- USD_per_token
+    """
+    cg_id = _coingeckoId(token_symbol)
+    cg_date = _coingeckoDate(timestr)
+    
+    cg = CoinGeckoAPI()
+    result = cg.get_coin_history_by_id(id=cg_id, date=cg_date)
+    rate = result["market_data"]["current_price"]["usd"]
+    return rate
+
+def _coingeckoId(token_symbol:str) -> str:
+    """Convert token_symbol to coingecko id for a few common tokens"""
+    id_ = token_symbol.lower()
+    if id_ == "btc":
+        return "bitcoin"
+    if "ocean" in id_:
+        return "ocean-protocol"
+    return id_
+
+def _coingeckoDate(timestr:str) -> str:
+    """
+    @arguments
+      timestr -- str in format "YYYY-MM-DD"
+    @return
+      coingecko_date -- str in format "DD-MM-YYYY"
+    """
+    dt = datetime.strptime(timestr, "%Y-%m-%d")
+    return f"{dt.day:02d}-{dt.month:02d}-{dt.year:04d}"
+    
--- util/test/test_getrate.py
+++ util/test/test_getrate.py
@@ -1,9 +1,32 @@
+import pytest
+from pytest import approx
+
 from util import getrate
 
-def test1():
-    nsamp = 5
-    seed = 32
-    rate = getrate.getrate(
-        "OCEAN", "2022-01-20_00:00", "2022-01-26_23:59", nsamp, seed)
-    assert 0.2 <= rate <= 2.00
+def test_getrate_ocean_oneday():
+    r = getrate.getrate("OCEAN", "2022-01-20", "2022-01-20")
+    assert r == approx(0.75, 0.1) #coingecko numbers
+    
+def test_getrate_ocean_oneweek():
+    r = getrate.getrate("OCEAN", "2022-01-20", "2022-01-26")
+    assert r == approx(0.65, 0.1) #coingecko numbers
+    
+def test_getrate_h2o():
+    for symbol in ["H2O", "H2o", "h2o"]:
+        assert getrate.getrate(symbol, "foo", "bar") == approx(1.618, 0.1)
+
+def test_start_after_fin():
+    with pytest.raises(ValueError):
+        getrate.getrate("OCEAN", "2021-01-26", "2021-12-20")
         
+def test_ratelimit():
+    with pytest.raises(ValueError):
+        getrate.getrate("OCEAN", "2021-01-20", "2021-12-26")
+        
+def test_coingeckoRate_bitcoin():
+    r = getrate.coingeckoRate("bitcoin", "2022-01-31")
+    assert r == approx(37983.15, 0.1)
+
+def test_coingeckoRate_ocean():
+    r = getrate.coingeckoRate("ocean-protocol", "2022-01-31")
+    assert r == approx(0.58, 0.1)
