132071ae56dfa39c2a59e5db9eecd8609b4366ae
trentmc <5305452+trentmc@users.noreply.github.com>
2022-04-07 10:52:49 +0200

    all calcrewards tests now passing

--- util/test/test_calcrewards.py
+++ util/test/test_calcrewards.py
@@ -24,7 +24,7 @@ def test_calcRewards2():
 def test_calcRewards3():
     stakes = {"ocean": {"pool1": {"LP1":1.0, "LP2":1.0},
                         "pool2": {"LP1":1.0, "LP3":1.0}}}
-    pool_vols = {"pool1":1.0} #pool1 has volume, but not pool2
+    pool_vols = {"ocean": {"pool1":1.0}} #pool1 has volume, but not pool2
     rewards = calcRewards(stakes, pool_vols, RATES, OCEAN_avail=10.0)
     assert sum(rewards.values()) == pytest.approx(10.0, 0.01)
     assert min(rewards.values()) > 0, "shouldn't have entries with 0 rewards"
@@ -34,7 +34,7 @@ def test_calcRewards3():
 def test_calcRewards4():
     stakes = {"ocean": {"pool1": {"LP1":1.0, "LP2":1.0},
                         "pool2": {"LP1":1.0, "LP3":1.0}}}
-    pool_vols = {"pool1":1.0, "pool2":1.0} #pool1 and 2 both have volume
+    pool_vols = {"ocean": {"pool1":1.0,"pool2":1.0}} #pool1 & 2 both have volume
     rewards = calcRewards(stakes, pool_vols, RATES, OCEAN_avail=10.0)
     assert sum(rewards.values()) == pytest.approx(10.0, 0.01)
     assert rewards == {"LP1":5.0, "LP2":2.5, "LP3":2.5}
