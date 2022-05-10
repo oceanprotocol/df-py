0ca7a2dc76cba189501beb255e32cbfc2df62dc9
trizin <25263018+trizin@users.noreply.github.com>
2022-05-10 09:32:33 +0000

    New contract

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -1,29 +1,85 @@
 pragma solidity 0.8.12;
 // SPDX-License-Identifier: (Apache-2.0 AND CC-BY-4.0)
 
-
 import "OpenZeppelin/openzeppelin-contracts@4.2.0/contracts/token/ERC20/IERC20.sol";
 import "OpenZeppelin/openzeppelin-contracts@4.2.0/contracts/token/ERC20/utils/SafeERC20.sol";
 import "OpenZeppelin/openzeppelin-contracts@4.2.0/contracts/security/ReentrancyGuard.sol";
 import "OpenZeppelin/openzeppelin-contracts@4.2.0/contracts/access/Ownable.sol";
 
 contract Airdrop is Ownable, ReentrancyGuard {
-
     using SafeERC20 for IERC20;
 
     event Allocated(address[] tos, uint256[] values);
     event Claimed(address to, uint256 value);
 
-    IERC20 public token;
+    // token address => user address => balance
+    mapping(address => mapping(address => uint256)) balances;
+    mapping(address => uint256) allocated;
 
-    mapping(address => uint256) balances;
+    // Caller calls token.safeApprove(contract_addr, sum(values)),
+    // then it calls this function. Anyone can call this, if can they fund it!
+    function allocate(
+        address[] calldata _tos,
+        uint256[] calldata _values,
+        address tokenAddress
+    ) external returns (bool) {
+        require(_tos.length == _values.length, "Lengths must match");
 
-    constructor(address token_address) public {
-	token = IERC20(token_address);
+        uint256 total_value = 0.0;
+        for (uint256 i = 0; i < _values.length; i += 1) {
+            require(_tos[i] != address(0), "Address invalid");
+            require(_values[i] > 0, "Value invalid");
+            balances[tokenAddress][_tos[i]] += _values[i];
+            total_value += _values[i];
+        }
+
+        IERC20(tokenAddress).safeTransferFrom(
+            msg.sender,
+            address(this),
+            total_value
+        );
+
+        allocated[tokenAddress] = allocated[tokenAddress] + total_value;
+
+        emit Allocated(_tos, _values);
+        return true;
     }
 
-    function getToken() public view returns (address) {
-	return address(token);
+    function claimable(address _to, address tokenAddress)
+        public
+        view
+        returns (uint256)
+    {
+        return balances[tokenAddress][_to];
+    }
+
+    function claimMultiple(address _to, address[] calldata tokenAddresses)
+        public
+    {
+        for (uint256 i = 0; i < tokenAddresses.length; i++) {
+            claimFor(_to, tokenAddresses[i]);
+        }
+    }
+
+    // Recipient claims for themselves
+    function claim(address[] calldata tokenAddresses) external returns (bool) {
+        claimMultiple(msg.sender, tokenAddresses);
+        return true;
+    }
+
+    // Others claim on behalf of recipient
+    function claimFor(address _to, address tokenAddress)
+        public
+        nonReentrant
+        returns (bool)
+    {
+        uint256 amt = balances[tokenAddress][_to];
+        require(amt > 0, "Nothing to claim");
+        balances[tokenAddress][_to] = 0;
+        IERC20(tokenAddress).safeTransfer(_to, amt);
+        allocated[tokenAddress] = allocated[tokenAddress] - amt;
+        emit Claimed(_to, amt);
+        return true;
     }
 
     /*
@@ -31,57 +87,16 @@ contract Airdrop is Ownable, ReentrancyGuard {
      * @param _amount The amount of tokens to withdraw.
      * @param _token The token address to withdraw.
      */
-    function withdrawERCToken(uint256 amount, address _token) external onlyOwner {
-        require(_token != address(token),"Cannot withdraw main token");
+    function withdrawERCToken(uint256 amount, address _token)
+        external
+        onlyOwner
+    {
+        require(allocated[_token] == 0, "Cannot withdraw allocated token");
         IERC20(_token).transfer(msg.sender, amount);
     }
 
-
     // Don't allow eth transfers
-    fallback() external
-    {
-        revert('Invalid ether transfer');
-    }
-
-    // Caller calls token.safeApprove(contract_addr, sum(values)),
-    // then it calls this function. Anyone can call this, if can they fund it!
-    function allocate(address[] calldata _tos, uint256[] calldata _values)
-	external returns (bool)
-    {
-	require(_tos.length == _values.length, "Lengths must match");
-
-	uint256 total_value = 0.0;
-	for (uint i = 0; i < _values.length; i += 1) {
-            require(_tos[i] != address(0), "Address invalid");
-            require(_values[i] > 0, "Value invalid");
-	    balances[_tos[i]] += _values[i];
-	    total_value += _values[i];
-        }
-
-	token.safeTransferFrom(msg.sender, address(this), total_value);
-
-	emit Allocated(_tos, _values);
-	return true;
-    }
-
-    function claimable(address _to) public view returns (uint256) {
-	return balances[_to];
-    }
-    
-    // Recipient claims for themselves
-    function claim() external returns (bool) {
-	claimFor(msg.sender);
-	return true;
-    }
-
-    // Others claim on behalf of recipient
-    function claimFor(address _to) public nonReentrant returns (bool) {
-	uint256 amt = balances[_to];
-	require (amt > 0, "Nothing to claim");
-	balances[_to] = 0;
-	token.safeTransfer(_to, amt);
-	emit Claimed(_to, amt);
-	return true;
+    fallback() external {
+        revert("Invalid ether transfer");
     }
 }
-    
