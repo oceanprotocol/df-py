00d9cc50b39ea3e51c70f3d528c69e6228af1038
trentmc <5305452+trentmc@users.noreply.github.com>
2022-05-11 12:37:21 +0200

    the account needed OCEAN. account0 has it by default, not account1

--- util/test/test_oceanutil.py
+++ util/test/test_oceanutil.py
@@ -7,8 +7,7 @@ from util.base18 import fromBase18, toBase18
 from util.constants import BROWNIE_PROJECT as B
 
 accounts = brownie.network.accounts
-account1 = accounts[1]
-addr1 = account1.address
+account0 = accounts[0]
 
 @enforce_types
 def test_recordDeployedContracts(ADDRESS_FILE):
@@ -23,25 +22,25 @@ def test_OCEANtoken(ADDRESS_FILE):
 @enforce_types
 def test_createDataNFT(ADDRESS_FILE):
     recordDeployedContracts(ADDRESS_FILE, "development")
-    data_NFT = createDataNFT("nft_name", "nft_symbol", account1)
+    data_NFT = createDataNFT("nft_name", "nft_symbol", account0)
     assert data_NFT.name() == "nft_name"
     assert data_NFT.symbol() == "nft_symbol"
     
 @enforce_types
 def test_createDatatokenFromDataNFT(ADDRESS_FILE):
     recordDeployedContracts(ADDRESS_FILE, "development")
-    data_NFT = createDataNFT("foo", "foo", account1)
+    data_NFT = createDataNFT("foo", "foo", account0)
     DT = createDatatokenFromDataNFT(
-        "dt_name", "dt_symbol", data_NFT, account1)
+        "dt_name", "dt_symbol", data_NFT, account0)
     assert DT.name() == "dt_name"
     assert DT.symbol() == "dt_symbol"
 
 @enforce_types
 def test_createBPoolFromDatatoken(ADDRESS_FILE):
     recordDeployedContracts(ADDRESS_FILE, "development")
-    data_NFT = createDataNFT("foo", "foo", account1)
-    DT = createDatatokenFromDataNFT("foo", "foo", data_NFT, account1)
-    pool = createBPoolFromDatatoken(DT, account1)
+    data_NFT = createDataNFT("foo", "foo", account0)
+    DT = createDatatokenFromDataNFT("foo", "foo", data_NFT, account0)
+    pool = createBPoolFromDatatoken(DT, account0)
 
 
     
