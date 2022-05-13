9b15e684d1184520f4dfa684b01fcfecfc661751
trentmc <5305452+trentmc@users.noreply.github.com>
2022-05-13 08:48:57 +0200

    begin test_dftool

---
+++ util/test/test_dftool.py
@@ -0,0 +1,48 @@
+import os
+from enforce_typing import enforce_types
+import types
+
+from util import constants, csvs
+
+
+def test_do_query(tmp_path):
+    prev = _setBargeEnvvars()
+    
+    CHAINID = 0
+    ST = 0
+    FIN = "latest"
+    NSAMP = 5
+    CSV_DIR = str(tmp_path)
+    
+    cmd = f"dftool query {CHAINID} {ST} {FIN} {NSAMP} {CSV_DIR}"
+    os.system(cmd)
+
+    assert csvs.stakesCsvFilenames(CSV_DIR)
+    assert csvs.poolvolsCsvFilenames(CSV_DIR)
+
+    _restorePrevEnvvars(prev)
+
+
+def _setBargeEnvvars():
+    prev = types.SimpleNamespace()
+    
+    prev.ADDRESS_FILE = os.environ.get('ADDRESS_FILE')
+    os.environ['ADDRESS_FILE'] = constants.BARGE_ADDRESS_FILE
+    
+    prev.SUBGRAPH_URI = os.environ.get('SUBGRAPH_URI')
+    os.environ['SUBGRAPH_URI'] = constants.BARGE_SUBGRAPH_URI
+
+    return prev
+
+
+def _restorePrevEnvvars(prev):
+    if prev.ADDRESS_FILE is None:
+        del os.environ['ADDRESS_FILE']
+    else:
+        os.environ['ADDRESS_FILE'] = prev.ADDRESS_FILE
+        
+    if prev.SUBGRAPH_URI is None:
+        del os.environ['SUBGRAPH_URI']
+    else:
+        os.environ['SUBGRAPH_URI'] = prev.SUBGRAPH_URI
+
