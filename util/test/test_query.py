d8a2cd45ed48be5b5f1d59b8e74ca1143e5c6d4a
trentmc <5305452+trentmc@users.noreply.github.com>
2022-05-18 11:42:45 +0200

    rewire testing, WIP, almost done

--- util/test/test_query.py
+++ util/test/test_query.py
@@ -1,7 +1,7 @@
 import brownie
 from enforce_typing import enforce_types
 from pprint import pprint
-import random
+import random, time
 
 from util import chainlist, query
 from util.blockrange import BlockRange
@@ -13,94 +13,107 @@ account0 = brownie.network.accounts[0]
 chain = brownie.network.chain
 
 CHAINID = 0
+   
 
-OCEAN = None
-CO2, CO2_SYM = None
+def test_all():
+    """Run this all as a single test, because we may have to
+    re-loop or sleep until the info we want is there."""
+
+    address_file = chainlist.chainIdToAddressFile(CHAINID)
+    recordDeployedContracts(address_file, CHAINID)
+    
+    OCEAN = OCEANtoken()
+    conftest.fillAccountsWithToken(OCEAN)
+        
+    CO2_SYM = f"CO2_{random.randint(0,99999):05d}"
+    CO2 = B.Simpletoken.deploy(
+        CO2_SYM, CO2_SYM, 18, 1e26, {"from": account0})
+    conftest.fillAccountsWithToken(CO2)
+
+    #keep deploying, until TheGraph node sees volume, or timeout
+    # (assumes that with volume, everything else is there too
+    for loop_i in range(100):
+        assert loop_i < 5, "timeout" 
+        if _volumeSeen(CO2_SYM):
+            break
+        conftest.randomDeployTokensAndPoolsThenConsume(2, OCEAN)
+        conftest.randomDeployTokensAndPoolsThenConsume(2, CO2)
+        print(f"loop_i={loop_i}")
+        time.sleep(2)
+
+    #run actual tests
+    _test_SimplePool(CO2)
+    _test_getApprovedTokens(CO2_SYM)
+    _test_pools(CO2_SYM)
+    _test_stakes(CO2_SYM)
+    _test_getDTVolumes(CO2_SYM)
+    _test_getPoolVolumes(CO2_SYM)
+
+
+def _volumeSeen(CO2_SYM):
+    st, fin = 1, len(chain)
+    DT_vols = query.getDTVolumes(st, fin, CHAINID)
+    if CO2_SYM not in DT_vols:
+        return False
+    
+    vol_OCEAN = sum(DT_vols["OCEAN"].values())
+    vol_CO2 =  sum(DT_vols[CO2_SYM].values())
+    return (vol_OCEAN > 0.0) and (vol_CO2 > 0.0)
+
 
 @enforce_types
-def test_SimplePool():
+def _test_SimplePool(CO2):
     pool = query.SimplePool(
         "0xpool_addr", "0xnft_addr", "0xdt_addr", "DT", CO2.address)
     assert "SimplePool" in str(pool)
 
 
 @enforce_types
-def test_getPools():
-    assert _poolsHaveToken("OCEAN")
-    assert _poolsHaveToken(CO2_SYM)
+def _test_getApprovedTokens(CO2_SYM:str):
+    approved_tokens = query.getApprovedTokens(CHAINID)
+    assert "OCEAN" in approved_tokens.values()
+    assert CO2_SYM not in approved_tokens.values()
+
+    
+@enforce_types
+def _test_pools(CO2_SYM:str):
+    pools = query.getPools(CHAINID)
+    assert [p for p in pools if p.basetoken_symbol == "OCEAN"]
+    assert [p for p in pools if p.basetoken_symbol == CO2_SYM]
 
 
 @enforce_types
-def test_getStakes():
+def _test_stakes(CO2_SYM:str):
     st, fin, n = 1, len(chain), 250
     rng = BlockRange(st, fin, n)
     pools = query.getPools(CHAINID)
     stakes = query.getStakes(pools, rng, CHAINID)
 
+    assert "OCEAN" in stakes, stakes.keys()
+    assert CO2_SYM in stakes, (CO2_SYM, stakes.keys())
+    
     for basetoken_symbol in ["OCEAN", CO2_SYM]:
-        assert basetoken_symbol in stakes
         for stakes_at_pool in stakes[basetoken_symbol].values():
             assert len(stakes_at_pool) > 0
             assert min(stakes_at_pool.values()) > 0.0
 
 
 @enforce_types
-def test_getDTVolumes():
+def _test_getDTVolumes(CO2_SYM:str):
     st, fin = 1, len(chain)
     DT_vols = query.getDTVolumes(st, fin, CHAINID)
+    assert "OCEAN" in DT_vols, DT_vols.keys()
+    assert CO2_SYM in DT_vols, (CO2_SYM, DT_vols.keys())
     assert sum(DT_vols["OCEAN"].values()) > 0.0
     assert sum(DT_vols[CO2_SYM].values()) > 0.0
 
 
 @enforce_types
-def test_getPoolVolumes():
+def _test_getPoolVolumes(CO2_SYM:str):
     pools = query.getPools(CHAINID)
     st, fin = 1, len(chain)
     poolvols = query.getPoolVolumes(pools, st, fin, CHAINID)
+    assert "OCEAN" in poolvols, poolvols.keys()
+    assert CO2_SYM in poolvols, (CO2_SYM, poolvols.keys())
     assert sum(poolvols["OCEAN"].values()) > 0.0
     assert sum(poolvols[CO2_SYM].values()) > 0.0
-
-
-@enforce_types
-def test_getApprovedTokens():
-    approved_tokens = query.getApprovedTokens(CHAINID)
-    assert "OCEAN" in approved_tokens.values()
-    assert CO2_SYM not in approved_tokens.values()
-    
-
-# ========================================================================
-@enforce_types
-def setup_module():
-    """Runs before each test"""
-
-    address_file = chainlist.chainIdToAddressFile(CHAINID)
-    recordDeployedContracts(address_file, CHAINID)
-    
-    global OCEAN
-    if OCEAN is None:
-        OCEAN = OCEANtoken()
-        conftest.fillAccountsWithToken(OCEAN)
-        
-    global CO2, CO2_SYM
-    if CO2 is None:
-        CO2_SYM = f"CO2_{random.randint(0,99999):05d}"
-        CO2 = B.Simpletoken.deploy(
-            CO2_SYM, CO2_SYM, 18, 1e26, {"from": account0})
-        conftest.fillAccountsWithToken(CO2)
-        
-    _randomDeployUntilHaveTokenOrTimeout("OCEAN")
-    _randomDeployUntilHaveTokenOrTimeout(CO2_SYM)
-
-
-def _deployEtcUntilHaveTokenOrTimeout(symbol:str):
-    for loop_i in range(10):
-        if _poolsHaveToken(symbol):
-            break
-        conftest.randomDeployTokensAndPoolsThenConsume(2, symbol)
-        time.sleep(2)
-
-
-def _poolsHaveToken(symbol:str) -> bool:
-    pools = query.getPools(CHAINID)
-    return bool([pool for pool in pools
-                 if p.basetoken_symbol == "OCEAN" for p in pools])
