2071dfca98e7a96a481b94c5a979439b1af2d50f
trentmc <5305452+trentmc@users.noreply.github.com>
2022-05-27 15:29:30 +0200

    tweak

--- util/test/test_blocktime.py
+++ util/test/test_blocktime.py
@@ -1,16 +1,21 @@
-import brownie
 from pytest import approx
+
+import brownie
+from enforce_typing import enforce_types
+
 from util.blocktime import timestrToBlock, timestrToTimestamp, timestampToBlock
 
 chain = brownie.network.chain
 
 
+@enforce_types
 def test_timestrToBlock_1():
     # tests here are light, the real tests are in test_*() below
     assert timestrToBlock(chain, "2022-03-29") >= 0.0
     assert timestrToBlock(chain, "2022-03-29_0:00") >= 0.0
 
 
+@enforce_types
 def test_timestampToBlock_FarLeft():
     b = timestrToBlock(chain, "1970-01-01")
     assert b == 0 and isinstance(b, int)
@@ -19,6 +24,7 @@ def test_timestampToBlock_FarLeft():
     assert b == 0 and isinstance(b, int)
 
 
+@enforce_types
 def test_timestampToBlock_FarRight():
     b = timestrToBlock(chain, "2030-01-01")
     assert b == len(chain) and isinstance(b, int)
@@ -27,6 +33,7 @@ def test_timestampToBlock_FarRight():
     assert b == len(chain) and isinstance(b, int)
 
 
+@enforce_types
 def test_timestrToTimestamp():
     t = timestrToTimestamp("1970-01-01_0:00")
     assert t == 0.0 and isinstance(t, float)
@@ -38,6 +45,7 @@ def test_timestrToTimestamp():
     assert t == 1648512000.0 and isinstance(t, float)
 
 
+@enforce_types
 def test_timestampToBlock():
     # gather timestamp and blocks at block offset 0, 9, 29
     timestamp0 = chain[-1].timestamp
