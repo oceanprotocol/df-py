4636a3cfc686d210b32cab33aba70e101b19ec4d
trentmc <5305452+trentmc@users.noreply.github.com>
2022-05-27 17:33:05 +0200

    bug fix

--- util/test/test_networkutil.py
+++ util/test/test_networkutil.py
@@ -6,7 +6,8 @@ from util import networkutil
 
 @enforce_types
 def test_chainIdToSubgraphUri():
-    assert networkutil.chainIdToSubgraphUri(0)[:21] == "http://127.0.0.1:9000"
+    assert networkutil.chainIdToSubgraphUri(networkutil.DEV_CHAINID)[:21] == \
+        "http://127.0.0.1:9000"
 
     for chainID in [1, 137]:
         with pytest.raises(NotImplementedError):
