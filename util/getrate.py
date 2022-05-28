378d0fb06352c4be55f45c2583c97a3e3c7c0c9f
trentmc <5305452+trentmc@users.noreply.github.com>
2022-05-28 06:09:14 +0200

    Tests passing

--- util/getrate.py
+++ util/getrate.py
@@ -1,56 +1,59 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+from typing import Tuple, Union
+
+from enforce_typing import enforce_types
 import requests
-from typing import Union
 
 
-def getrate(token_symbol: str, st: str, fin: str) -> float:
+@enforce_types
+def getrate(token_symbol: str, st: str, fin: str) -> Union[float, None]:
     """
     @description
-      Get the exchange rate for a token.
-      Takes one measure per day. Averages across each day.
-      Max 40 days, since Coingecko API rate-limits to 50 calls/min.
+      Get the exchange rate for a token. Uses Binance. Coingecko is backup.
 
     @arguments
       token_symbol -- str -- e.g. "OCEAN" or "H2O"
       st -- start date in format "YYYY-MM-DD"
-      fin -- end date in format "YYYY-MM-DD"
+      fin -- end date
 
     @return
-      rate -- float -- USD_per_token
+      rate -- float or None -- USD_per_token. None if failure
     """
-    #corner case
-    if token_symbol.lower() == "h2o":
-        return 1.618
-
-    st_dt = datetime.strptime(st, "%Y-%m-%d")
-    fin_dt = datetime.strptime(fin, "%Y-%m-%d")
-    num_days = (fin_dt - st_dt).days
-    if num_days < 0:
-        raise ValueError("Start date is after end date")
-
-    rate = binanceRate(token_symbol, st_dt, fin_dt)
+    rate = getBinanceRate(token_symbol, st, fin)
     if rate is not None:
         return rate
 
     print("Couldn't get Binance data; trying CoinGecko")
-    rate = coingeckoRate(token_symbol, st_dt, fin_dt)
+    rate = getCoingeckoRate(token_symbol, st, fin)
     if rate is not None:
         return rate
 
-    print("Couldn't get CoinGecko data")
-    raise Exception(f"Couldn't get rate for {token_symbol}")
+    print("Couldn't get CoinGecko data. Returning None")
+    return None
 
 
-def binanceRate(token_symbol: str, st_dt: datetime, fin_dt: datetime) \
-    -> Union[float, None]:
+@enforce_types
+def getBinanceRate(token_symbol: str, st: str, fin: str) -> Union[float, None]:
     """
     @arguments
       token_symbol -- e.g. "OCEAN", "BTC"
-      timestr -- str in format "YYYY-MM-DD"
+      st -- start date in format "YYYY-MM-DD"
+      fin -- end date
     @return
       rate -- float or None -- USD_per_token. None if failure
     """
-    req_s = f"https://api.binance.com/api/v3/klines?symbol={token_symbol}USDT&interval=1d&startTime={int(st_dt.timestamp())*1000}&endTime={int(fin_dt.timestamp())*1000}" # pylint: disable=line-too-long
+    # corner case
+    if token_symbol.upper() == "H2O":
+        return 1.618
+
+    (st_dt, fin_dt) = _toDatetime(st, fin)
+    num_days = (fin_dt - st_dt).days
+    if num_days < 0:
+        raise ValueError("Start date is after end date")
+    if num_days == 0:  # binance needs >=1 days of data
+        st_dt = st_dt - timedelta(days=1)
+
+    req_s = f"https://api.binance.com/api/v3/klines?symbol={token_symbol}USDT&interval=1d&startTime={int(st_dt.timestamp())*1000}&endTime={int(fin_dt.timestamp())*1000}"  # pylint: disable=line-too-long
     res = requests.get(req_s)
     data = res.json()
     if data == []:
@@ -59,17 +62,29 @@ def binanceRate(token_symbol: str, st_dt: datetime, fin_dt: datetime) \
     return avg
 
 
-def coingeckoRate(token_symbol: str, st_dt: datetime, fin_dt: datetime) \
-    -> Union[float, None]:
+@enforce_types
+def getCoingeckoRate(token_symbol: str, st: str, fin: str) -> Union[float, None]:
     """
     @arguments
       token_symbol -- e.g. "OCEAN", "BTC"
-      timestr -- str in format "YYYY-MM-DD"
+      st -- start date in format "YYYY-MM-DD"
+      fin -- end date
     @return
       rate -- float or None -- USD_per_token. None if failure
     """
+    # corner case
+    if token_symbol.upper() == "H2O":
+        return 1.618
+
+    (st_dt, fin_dt) = _toDatetime(st, fin)
+    num_days = (fin_dt - st_dt).days
+    if num_days < 0:
+        raise ValueError("Start date is after end date")
+    if num_days == 0:  # coingecko needs >=1 days of data
+        st_dt = st_dt - timedelta(days=1)
+
     cg_id = _coingeckoId(token_symbol)
-    req_s = f"https://api.coingecko.com/api/v3/coins/{cg_id}/market_chart/range?vs_currency=usd&from={int(st_dt.timestamp())}&to={int(fin_dt.timestamp())}" # pylint: disable=line-too-long
+    req_s = f"https://api.coingecko.com/api/v3/coins/{cg_id}/market_chart/range?vs_currency=usd&from={int(st_dt.timestamp())}&to={int(fin_dt.timestamp())}"  # pylint: disable=line-too-long
     res = requests.get(req_s)
     data = res.json()["prices"]
     if data == []:
@@ -78,6 +93,21 @@ def coingeckoRate(token_symbol: str, st_dt: datetime, fin_dt: datetime) \
     return avg
 
 
+@enforce_types
+def _toDatetime(st: str, fin: str) -> Tuple[datetime, datetime]:
+    """
+    @arguments
+      st, fin -- (start date, end date) in format "YYYY-MM-DD"
+
+    @return
+      st_dt, fin_dt -- (start date, end date) in datetime
+    """
+    st_dt = datetime.strptime(st, "%Y-%m-%d")
+    fin_dt = datetime.strptime(fin, "%Y-%m-%d")
+    return (st_dt, fin_dt)
+
+
+@enforce_types
 def _coingeckoId(token_symbol: str) -> str:
     """Convert token_symbol to coingecko id for a few common tokens"""
     id_ = token_symbol.lower()
