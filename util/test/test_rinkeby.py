f4a9e1bb497305c8bbba0481ffdd699c668813c7
trentmc <5305452+trentmc@users.noreply.github.com>
2022-05-30 17:45:34 +0200

    test connecting to rinkeby, while bypassing brownie bug

--- util/test/test_rinkeby.py
+++ util/test/test_rinkeby.py
@@ -1,12 +1,8 @@
-import brownie
+import os
+
 from enforce_typing import enforce_types
-from pprint import pprint
-import pytest
 
-from util.query import submitQuery
-
-from util import networkutil, oceanutil, oceantestutil
-#from util.graphutil import submitQuery
+from util import networkutil
 
 CHAINID = networkutil.networkToChainId("rinkeby")
 
@@ -21,31 +17,24 @@ def test_chainIdToSubgraphUri():
     assert "subgraph.rinkeby.oceanprotocol.com" in uri
     
 @enforce_types
-def test_main():
-    #FIXME: only do a bash level call here, otherwise brownie goes crazy
-
-    
-    return
-    #problem, like in https://github.com/eth-brownie/brownie/issues/1144
+def test_main(tmp_path):
+    #brownie has a bug when trying to shut down non-ganache networks
+    #details in https://github.com/eth-brownie/brownie/issues/1144
     #in file venv/lib/python3.8/site-packages/brownie/network/state.py
     #   function: _remove_contract()
     #   code: del _contract_map[contract.address]
-
     
-    #setup_function
-    networkutil.connect(CHAINID)
-    address_file = networkutil.chainIdToAddressFile(CHAINID)
-    oceanutil.recordDeployedContracts(address_file)
+    #This isn't a problem from cli, since it cleans up mem
+    #But it is a problem for unit tests. Our workaround is to do a system call.
 
-    #main
-    assert brownie.network.chain.id == CHAINID
-    
-    query = "{ opcs{approvedTokens} }"
-    result = submitQuery(query, CHAINID)
-    
-    OCEAN = oceanutil.OCEANtoken()
-    assert OCEAN.symbol() == "OCEAN"
+    ACCOUNT_ADDR = '0xc945a5a960fef1a9c3fef8593fc2446d1d7c6146'
+    TOKEN_ADDR = '0xddea378a6ddc8afec82c36e9b0078826bf9e68b6'
+    fn = os.path.join(tmp_path, "out.txt")
+    cmd = f"./dftool acctinfo {CHAINID} {ACCOUNT_ADDR} {TOKEN_ADDR}>{fn} 2>{fn}"
+    os.system(cmd)
 
-    #teardown_function
-    networkutil.disconnect()
+    f = open(fn, 'r')
+    s = f.read()
+    f.close()
+    assert "62000.00000000001 ZRX" in s
 
