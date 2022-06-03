ffb97b8f048c54d3baaa83d127d70afabca11f67
trizin <25263018+trizin@users.noreply.github.com>
2022-06-03 10:32:19 +0000

    Lowercase address & assert in cleancase

--- util/cleancase.py
+++ util/cleancase.py
@@ -2,7 +2,7 @@
 # 'assertX' functions asserts that 'X' follows the rules
 
 FAKE_CHAINID = 99
-FAKE_TOKEN = "FAKE_TOKEN"
+FAKE_TOKEN = "fake_token"
 
 
 def modStakes(stakes: dict) -> dict:
@@ -11,15 +11,15 @@ def modStakes(stakes: dict) -> dict:
     for chainID in stakes:
         chainID2 = chainID
         stakes2[chainID2] = {}
-        for basetoken in stakes[chainID]:
-            basetoken2 = basetoken.upper()
-            stakes2[chainID2][basetoken2] = {}
-            for pool_addr in stakes[chainID][basetoken]:
+        for baseaddr in stakes[chainID]:
+            baseaddr2 = baseaddr.lower()
+            stakes2[chainID2][baseaddr2] = {}
+            for pool_addr in stakes[chainID][baseaddr]:
                 pool_addr2 = pool_addr.lower()
-                stakes2[chainID2][basetoken2][pool_addr2] = {}
-                for LP_addr, st in stakes[chainID][basetoken][pool_addr].items():
+                stakes2[chainID2][baseaddr2][pool_addr2] = {}
+                for LP_addr, st in stakes[chainID][baseaddr][pool_addr].items():
                     LP_addr2 = LP_addr.lower()
-                    stakes2[chainID2][basetoken2][pool_addr2][LP_addr2] = st
+                    stakes2[chainID2][baseaddr2][pool_addr2][LP_addr2] = st
 
     assertStakes(stakes2)
     return stakes2
@@ -29,7 +29,8 @@ def assertStakes(stakes: dict):
     """stakes - dict of [chainID][basetoken_sym][pool_addr][LP_addr] : stake"""
     for chainID in stakes:
         for basetoken in stakes[chainID]:
-            assert basetoken == basetoken.upper(), basetoken
+            print("b", basetoken, basetoken.lower())
+            assert basetoken == basetoken.lower(), basetoken
             for pool_addr in stakes[chainID][basetoken]:
                 assert pool_addr == pool_addr.lower(), pool_addr
                 for LP_addr in stakes[chainID][basetoken][pool_addr]:
@@ -58,12 +59,12 @@ def modPoolvols(poolvols: dict) -> dict:
     for chainID in poolvols:
         chainID2 = chainID
         poolvols2[chainID2] = {}
-        for basetoken in poolvols[chainID]:
-            basetoken2 = basetoken.upper()
-            poolvols2[chainID2][basetoken2] = {}
-            for pool_addr, vol in poolvols[chainID][basetoken].items():
+        for baseaddr in poolvols[chainID]:
+            baseaddr2 = baseaddr.lower()
+            poolvols2[chainID2][baseaddr2] = {}
+            for pool_addr, vol in poolvols[chainID][baseaddr].items():
                 pool_addr2 = pool_addr.lower()
-                poolvols2[chainID2][basetoken2][pool_addr2] = vol
+                poolvols2[chainID2][baseaddr2][pool_addr2] = vol
 
     assertPoolvols(poolvols2)
     return poolvols2
@@ -73,7 +74,9 @@ def assertPoolvols(poolvols: dict):
     """poolvols - dict of [chainID][basetoken_symbol][pool_addr] : vol"""
     for chainID in poolvols:
         for basetoken in poolvols[chainID]:
-            assert basetoken == basetoken.upper(), basetoken
+            print(basetoken)
+            print(basetoken.lower())
+            assert basetoken == basetoken.lower(), basetoken
             for pool_addr in poolvols[chainID][basetoken]:
                 assert pool_addr == pool_addr.lower(), pool_addr
 
