4970bf9522ae219ea5aca7ebd324057cce7e5093
Berkay Saglam <25263018+trizin@users.noreply.github.com>
2022-06-09 13:49:47 +0300

    Update `MARKET_ASSET_BASE_URL` (#124)

--- util/constants.py
+++ util/constants.py
@@ -2,7 +2,7 @@ import brownie
 
 ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
 
-MARKET_ASSET_BASE_URL = "https://v4.market.oceanprotocol.com/asset/"
+MARKET_ASSET_BASE_URL = "https://market.oceanprotocol.com/asset/"
 
 BROWNIE_PROJECT = brownie.project.load("./", name="MyProject")
 
