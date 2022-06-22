c974154ba21fd1859f89f818f04f6c244df1e5a3
trentmc <gtrent@gmail.com>
2022-06-22 22:45:55 +0200

    more functionality for tok.py

--- util/test/test_tok.py
+++ util/test/test_tok.py
@@ -34,24 +34,55 @@ def test_0x():
 
 @enforce_types
 def test_TokSet():
-    l = [Tok(1, "0x123", "OCEAN"),
-         Tok(1, "0x456", "H2O"),
-         Tok(2, "0x789", "OCEAN")]
-    tok_set = TokSet(l)
+    tok_set = TokSet()
+    tok_set.add(Tok(1, "0x123", "OCEAN"))
+    tok_set.add(Tok(1, "0x456", "H2O"))
+    tok_set.add(Tok(2, "0x78b", "OCEAN"))
     
     assert tok_set.hasAddress(1, "0x123")
-    assert tok_set.hasAddress(2, "0x789")
-    assert not tok_set.hasAddress(9, "0x789")
-    
-    assert tok_set.symbol(1, "0x123") == "OCEAN"
-    assert tok_set.symbol(1, "0x456") == "H2O"
+    assert tok_set.hasAddress(2, "0x78b")
+    assert not tok_set.hasAddress(9, "0x78b")
     with pytest.raises(AssertionError):
-        tok_set.symbol(9, "0x789")
+        tok_set.hasAddress(1, "123") # missing "0x"
+    with pytest.raises(AssertionError):
+        tok_set.hasAddress(2, "0x78B") # unwanted uppercase in address
+
+    assert tok_set.hasSymbol(1, "OCEAN")
+    assert tok_set.hasSymbol(1, "H2O")
+    assert not tok_set.hasSymbol(9, "OCEAN")
+    with pytest.raises(AssertionError):
+        tok_set.hasAddress(1, "ocEaN") # unwanted lowercase in address
+    
+    assert tok_set.getSymbol(1, "0x123") == "OCEAN"
+    assert tok_set.getSymbol(1, "0x456") == "H2O"
+    with pytest.raises(AssertionError):
+        tok_set.getSymbol(2, "0x78B") # unwanted uppercase in address
+    with pytest.raises(AssertionError):
+        tok_set.getSymbol(9, "0x78b") # only query what tokens exist
+    
+    assert tok_set.getAddress(1, "OCEAN") == "0x123"
+    assert tok_set.getAddress(2, "OCEAN") == "0x78b" #diff't address on diff't chain
+    assert tok_set.getAddress(1, "H2O") == "0x456"
+    with pytest.raises(AssertionError):
+        tok_set.getAddress(9, "OCEAN") # only query what tokens exist
+    with pytest.raises(AssertionError):
+        tok_set.getAddress(1, "ocEaN") # unwanted lowercase in symbol
 
     tok = tok_set.tokAtAddress(1, "0x123")
     assert tok.chainID == 1
     assert tok.address == "0x123"
     assert tok.symbol == "OCEAN"
+    assert tok_set.tokAtAddress(1, "0xfoo") is None
+    with pytest.raises(AssertionError):
+        tok_set.tokAtAddress(2, "0x78B") # unwanted uppercase in symbol
+
+    tok = tok_set.tokAtSymbol(1, "OCEAN")
+    assert tok.chainID == 1
+    assert tok.address == "0x123"
+    assert tok.symbol == "OCEAN"
+    assert tok_set.tokAtSymbol(1, "FOO") is None
+    with pytest.raises(AssertionError):
+        tok_set.tokAtSymbol(1, "oCeAn") # unwanted lowercase in symbol
         
         
         
--- util/tok.py
+++ util/tok.py
@@ -17,25 +17,54 @@ class Tok:
         self.symbol = symbol
         
 class TokSet:
-    def __init__(self, toks: Set[Tok]):
-        self.toks = toks
+    def __init__(self):
+        self.toks = set()
+
+    def add(self, tok: Tok):
+        """
+        Add another Tok to this set. 
+        For a given chain: both address & symbol must be unique.
+        """
+        assert not self.hasAddress(tok.chainID, tok.address)
+        assert not self.hasSymbol(tok.chainID, tok.symbol)
+        self.toks.add(tok)
 
     def hasAddress(self, chainID: int, address: str) -> bool:
         """Is there a token at this chainID & address?"""
         tok = self.tokAtAddress(chainID, address)
         return (tok is not None)
 
-    def symbol(self, chainID: int, address: str) -> str:
+    def hasSymbol(self, chainID: int, symbol: str) -> bool:
+        """Is there a token at this chainID & address?"""
+        tok = self.tokAtSymbol(chainID, symbol)
+        return (tok is not None)
+
+    def getSymbol(self, chainID: int, address: str) -> str:
         """Returns Tok's symbol if there's a token, otherwise raises an error"""
         tok = self.tokAtAddress(chainID, address)
         assert tok is not None
         return tok.symbol
 
+    def getAddress(self, chainID: int, symbol: str) -> str:
+        """Returns Tok's address if there's a token, otherwise raises an error"""
+        tok = self.tokAtSymbol(chainID, symbol)
+        assert tok is not None
+        return tok.address
+
     def tokAtAddress(self, chainID: int, address: str) -> Union[Tok, None]:
         """Returns Tok if there's a token, otherwise returns None"""
         assert address == address.lower()
+        assert address[:2] == "0x"
         for tok in self.toks:
             if tok.chainID == chainID and tok.address == address:
+                return tok
+        return None
+
+    def tokAtSymbol(self, chainID: int, symbol: str) -> Union[Tok, None]:
+        """Returns Tok if there's a token, otherwise returns None"""
+        assert symbol == symbol.upper()
+        for tok in self.toks:
+            if tok.chainID == chainID and tok.symbol == symbol:
                 return tok
         return None
         
