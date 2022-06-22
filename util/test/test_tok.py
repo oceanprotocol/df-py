11fd960304d06a900981db380d4354281c0b73dc
trentmc <gtrent@gmail.com>
2022-06-22 22:23:26 +0200

    add TokSet, test it

--- util/test/test_tok.py
+++ util/test/test_tok.py
@@ -1,7 +1,7 @@
 from enforce_typing import enforce_types
 import pytest
 
-from util.tok import Tok
+from util.tok import Tok, TokSet
 
 @enforce_types
 def test_happy():
@@ -9,13 +9,13 @@ def test_happy():
 
 @enforce_types
 def test_types():
-    with pytest.raises(AssertionError):
+    with pytest.raises(TypeError):
         tok = Tok("not an int", "0x12ab3", "OCEAN")
         
-    with pytest.raises(AssertionError):
+    with pytest.raises(TypeError):
         tok = Tok(1, 100, "OCEAN")
         
-    with pytest.raises(AssertionError):
+    with pytest.raises(TypeError):
         tok = Tok(1, "0x12ab3", 322)
         
 @enforce_types
@@ -30,6 +30,28 @@ def test_casing():
 def test_0x():
     with pytest.raises(AssertionError):
         tok = Tok(1, "12ab3", "OCEAN")
+
+
+@enforce_types
+def test_TokSet():
+    l = [Tok(1, "0x123", "OCEAN"),
+         Tok(1, "0x456", "H2O"),
+         Tok(2, "0x789", "OCEAN")]
+    tok_set = TokSet(l)
+    
+    assert tok_set.hasAddress(1, "0x123")
+    assert tok_set.hasAddress(2, "0x789")
+    assert not tok_set.hasAddress(9, "0x789")
+    
+    assert tok_set.symbol(1, "0x123") == "OCEAN"
+    assert tok_set.symbol(1, "0x456") == "H2O"
+    with pytest.raises(AssertionError):
+        tok_set.symbol(9, "0x789")
+
+    tok = tok_set.tokAtAddress(1, "0x123")
+    assert tok.chainID == 1
+    assert tok.address == "0x123"
+    assert tok.symbol == "OCEAN"
         
         
         
--- util/tok.py
+++ util/tok.py
@@ -1,3 +1,8 @@
+# This is a class to help manage a token tuple (chainID, address, symbol)
+# E.g. for approved tokens
+
+from typing import Set, Union
+
 from enforce_typing import enforce_types
 
 @enforce_types
@@ -11,4 +16,27 @@ class Tok:
         self.address = address
         self.symbol = symbol
         
+class TokSet:
+    def __init__(self, toks: Set[Tok]):
+        self.toks = toks
+
+    def hasAddress(self, chainID: int, address: str) -> bool:
+        """Is there a token at this chainID & address?"""
+        tok = self.tokAtAddress(chainID, address)
+        return (tok is not None)
+
+    def symbol(self, chainID: int, address: str) -> str:
+        """Returns Tok's symbol if there's a token, otherwise raises an error"""
+        tok = self.tokAtAddress(chainID, address)
+        assert tok is not None
+        return tok.symbol
+
+    def tokAtAddress(self, chainID: int, address: str) -> Union[Tok, None]:
+        """Returns Tok if there's a token, otherwise returns None"""
+        assert address == address.lower()
+        for tok in self.toks:
+            if tok.chainID == chainID and tok.address == address:
+                return tok
+        return None
         
+
