4dca9a395db2e86ffbc3f89f371ee79eb48ef12d
trentmc <gtrent@gmail.com>
2022-06-23 11:59:00 +0200

    tweak

--- util/test/test_tok.py
+++ util/test/test_tok.py
@@ -6,34 +6,34 @@ from util.tok import Tok, TokSet
 
 @enforce_types
 def test_happy():
-    tok = Tok(1, "0x12ab3", "OCEAN")
+    Tok(1, "0x12ab3", "OCEAN")
 
 
 @enforce_types
 def test_types():
     with pytest.raises(TypeError):
-        tok = Tok("not an int", "0x12ab3", "OCEAN")
+        Tok("not an int", "0x12ab3", "OCEAN")
 
     with pytest.raises(TypeError):
-        tok = Tok(1, 100, "OCEAN")
+        Tok(1, 100, "OCEAN")
 
     with pytest.raises(TypeError):
-        tok = Tok(1, "0x12ab3", 322)
+        Tok(1, "0x12ab3", 322)
 
 
 @enforce_types
 def test_casing():
     with pytest.raises(AssertionError):
-        tok = Tok(1, "0x12Ab3", "OCEAN")
+        Tok(1, "0x12Ab3", "OCEAN")
 
     with pytest.raises(AssertionError):
-        tok = Tok(1, "0x12ab3", "OcEaN")
+        Tok(1, "0x12ab3", "OcEaN")
 
 
 @enforce_types
 def test_0x():
     with pytest.raises(AssertionError):
-        tok = Tok(1, "12ab3", "OCEAN")
+        Tok(1, "12ab3", "OCEAN")
 
 
 @enforce_types
