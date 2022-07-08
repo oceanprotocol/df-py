125369d712db852e55edda21e6d951782a7b1b4a
trentmc <gtrent@gmail.com>
2022-07-08 19:33:50 +0200

    Fix #213 - In DF3 dispense, poolinfo-137.csv had no entries

--- util/csvs.py
+++ util/csvs.py
@@ -496,20 +496,20 @@ def savePoolinfoCsv(
             for pool_addr in pools_by_addr:
                 assertIsEthAddr(pool_addr)
 
-                if pool_addr not in stakes_at_chain[basetoken_addr]:
-                    continue
-
                 p = pools_by_addr[pool_addr]
-                if p.basetoken_symbol not in rates:
-                    continue
 
                 did = oceanutil.calcDID(p.nft_addr, chainID)
                 url = constants.MARKET_ASSET_BASE_URL + did
 
-                stake_amt_BASE = sum(
-                    stakes_at_chain[basetoken_addr][pool_addr].values()
-                )
-                stake_amt_USD = stake_amt_BASE * rates[p.basetoken_symbol]
+                stake_amt_BASE = 0.0
+                if pool_addr in stakes_at_chain[basetoken_addr]:
+                    stake_amt_BASE = sum(
+                        stakes_at_chain[basetoken_addr][pool_addr].values()
+                    )
+
+                stake_amt_USD = ""
+                if p.basetoken_symbol in rates:
+                    stake_amt_USD = stake_amt_BASE * rates[p.basetoken_symbol]
 
                 vol_amt_BASE = 0.0
                 if (basetoken_addr in poolvols_at_chain) and (
@@ -517,7 +517,9 @@ def savePoolinfoCsv(
                 ):
                     vol_amt_BASE = poolvols_at_chain[basetoken_addr][pool_addr]
 
-                vol_amt_USD = vol_amt_BASE * rates[p.basetoken_symbol]
+                vol_amt_USD = ""
+                if p.basetoken_symbol in rates:
+                    vol_amt_USD = vol_amt_BASE * rates[p.basetoken_symbol]
 
                 row = [
                     str(chainID),
