b08eec960742c444d18513409d948c5a8aa6042f
trentmc <gtrent@gmail.com>
2022-07-08 11:45:33 +0200

    Rewrite tousd to use add_rates

--- util/tousd.py
+++ util/tousd.py
@@ -7,34 +7,62 @@ from util.tok import TokSet
 
 
 @enforce_types
-def stakesToUsd(stakes: dict, rates: Dict[str, float], tok_set: TokSet) -> dict:
+def ratesToAddrRates(
+        rates: Dict[str, float],
+        symbols: Dict[int, Dict[str,str]],
+) -> dict:
     """
     @description
       Converts stake values to be USD-denominated.
 
     @arguments
-      stakes - dict of [chainID][basetoken_address][pool_addr][LP_addr] : stake
-      rates - dict of [basetoken_symbol] : USD_per_basetoken
-      tok_set - TokSet
+      rates - dict of [token_symbol] : USD_price
+      symbols -- dict of [chainID][token_addr] : token_symbol
+
+    @return
+      addr_rates -- dict of [chainID][token_addr] : USD_price
+    """
+    addr_rates: dict = {}
+    for chainID in symbols:
+        addr_rates[chainID] = {}
+        for token_addr, token_symbol in symbols[chainID].items():
+            if token_symbol in rates:
+                addr_rates[chainID][token_addr] = rates[token_symbol]
+    return addr_rates
+    
+@enforce_types
+def stakesToUsd(
+        stakes: dict,
+        symbols: Dict[int, Dict[str,str]],
+        rates: Dict[str, float],
+) -> dict:
+    """
+    @description
+      Converts stake values to be USD-denominated.
+
+    @arguments
+      stakes - dict of [chainID][basetoken_addr][pool_addr][LP_addr] : stake
+      symbols -- dict of [chainID][basetoken_addr] : basetoken_symbol
+      rates - dict of [basetoken_symbol] : USD_price
 
     @return
       stakes_USD -- dict of [chainID][pool_addr][LP_addr] : stake_USD
     """
     cleancase.assertStakes(stakes)
     cleancase.assertRates(rates)
+    addr_rates = ratesToAddrRates(rates, symbols) # dict of [chainID][basetoken_addr] : USD_price
 
     stakes_USD: dict = {}
     for chainID in stakes:
         stakes_USD[chainID] = {}
-        for base_symb, rate in rates.items():
-            if not tok_set.hasSymbol(chainID, base_symb):
+        for basetoken_addr in stakes[chainID]:
+            rate = addr_rates[chainID][basetoken_addr]
+            if basetoken_addr not in stakes[chainID]:
                 continue
-            base_addr = tok_set.getAddress(chainID, base_symb)
-            if base_addr not in stakes[chainID]:
-                continue
-            for pool_addr in stakes[chainID][base_addr].keys():
-                stakes_USD[chainID][pool_addr] = {}
-                for LP_addr, stake in stakes[chainID][base_addr][pool_addr].items():
+            for pool_addr in stakes[chainID][basetoken_addr]:
+                if pool_addr not in stakes_USD[chainID]:
+                    stakes_USD[chainID][pool_addr] = {}
+                for LP_addr, stake in stakes[chainID][basetoken_addr][pool_addr].items():
                     stakes_USD[chainID][pool_addr][LP_addr] = stake * rate
 
     cleancase.assertStakesUsd(stakes_USD)
@@ -42,32 +70,35 @@ def stakesToUsd(stakes: dict, rates: Dict[str, float], tok_set: TokSet) -> dict:
 
 
 @enforce_types
-def poolvolsToUsd(poolvols: dict, rates: Dict[str, float], tok_set: TokSet) -> dict:
+def poolvolsToUsd(
+        poolvols: dict,
+        symbols: Dict[int, Dict[str,str]],
+        rates: Dict[str, float],
+) -> dict:
     """
     @description
       For a given chain, converts volume values to be USD-denominated.
 
     @arguments
       poolvols -- dict of [chainID][basetoken_address][pool_addr] : vol
-      rates -- dict of [basetoken_symbol] : USD_per_basetoken
-      tok_set - TokSet
+      symbols -- dict of [chainID][basetoken_addr] : basetoken_symbol
+      rates - dict of [basetoken_symbol] : USD_price
 
     @return
       poolvols_USD -- dict of [chainID][pool_addr] : vol_USD
     """
     cleancase.assertPoolvols(poolvols)
     cleancase.assertRates(rates)
+    addr_rates = ratesToAddrRates(rates, symbols) # dict of [chainID][basetoken_addr] : USD_price
 
     poolvols_USD: dict = {}
     for chainID in poolvols:
         poolvols_USD[chainID] = {}
-        for base_symb, rate in rates.items():
-            if not tok_set.hasSymbol(chainID, base_symb):
+        for basetoken_addr in poolvols[chainID]:
+            rate = addr_rates[chainID][basetoken_addr]        
+            if basetoken_addr not in poolvols[chainID]:
                 continue
-            base_addr = tok_set.getAddress(chainID, base_symb)
-            if base_addr not in poolvols[chainID]:
-                continue
-            for pool_addr, vol in poolvols[chainID][base_addr].items():
+            for pool_addr, vol in poolvols[chainID][basetoken_addr].items():
                 poolvols_USD[chainID][pool_addr] = vol * rate
 
     cleancase.assertPoolvolsUsd(poolvols_USD)
