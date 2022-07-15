df1fabcb7c5b7d3d3a378e200d828aa96b2baaaf
Berkay Saglam <25263018+trizin@users.noreply.github.com>
2022-07-15 14:38:20 +0300

    veOcean delegation system (#220)
    * Add delegation contracts
    
    * Rename contract
    
    * Add `test_delegation`

---
+++ util/test/veOcean/test_delegation.py
@@ -0,0 +1,93 @@
+import time
+import brownie
+from enforce_typing import enforce_types
+from pytest import approx
+from brownie import convert
+
+from util import networkutil, oceanutil
+from util.constants import BROWNIE_PROJECT as B
+from util.base18 import toBase18
+
+accounts = None
+alice = None
+bob = None
+veOcean = None
+veBoost = None
+ocean = None
+WEEK = 7 * 86400
+MAXTIME = 4 * 365 * 86400  # 4 years
+chain = brownie.network.chain
+TA = 10e18
+
+
+@enforce_types
+def test_alice_creates_boost():
+    """sending native tokens to dfrewards contract should revert"""
+    veOcean.checkpoint()
+    ocean.approve(veOcean.address, TA, {"from": alice})
+
+    t0 = chain.time()
+    t1 = t0 // WEEK * WEEK
+    t2 = t1 + WEEK * 5
+    chain.sleep(t1 - t0)
+
+    assert ocean.balanceOf(alice) != 0
+    veOcean.create_lock(TA, t2, {"from": alice})
+    assert ocean.balanceOf(alice) == 0
+
+    token_id = convert.to_uint(alice.address) << 96
+
+    # return print(veOcean.locked__end(alice), t2, t1)
+
+    veBoost.create_boost(
+        alice,
+        bob,
+        10_000,
+        0,
+        veOcean.locked__end(alice),
+        0,
+        {"from": alice},
+    )  # 10_000 is max percentage
+
+    with brownie.multicall(block_identifier=chain.height):
+        alice_adj_balance = veBoost.adjusted_balance_of(alice)
+        bob_adj_balance = veBoost.adjusted_balance_of(bob)
+
+        alice_delegated_boost = veBoost.delegated_boost(alice)
+        bob_received_boost = veBoost.received_boost(bob)
+
+        alice_vecrv_balance = veOcean.balanceOf(alice)
+        token_boost_value = veBoost.token_boost(token_id)
+
+    assert alice_adj_balance == 0
+    assert bob_adj_balance == alice_vecrv_balance
+    assert bob_received_boost == alice_delegated_boost
+    assert token_boost_value == alice_delegated_boost
+    assert veBoost.token_expiry(token_id) == (t2 // WEEK) * WEEK
+
+
+@enforce_types
+def setup_function():
+    global accounts, alice, bob, veOcean, ocean, veBoost
+    networkutil.connect(networkutil.DEV_CHAINID)
+    oceanutil.recordDevDeployedContracts()
+    accounts = brownie.network.accounts
+
+    alice = accounts.add()
+    bob = accounts.add()
+
+    ocean = oceanutil.OCEANtoken()
+    veOcean = B.veOcean.deploy(
+        ocean.address, "veOcean", "veOcean", "0.1.0", {"from": alice}
+    )
+
+    veBoost = B.veBoost.deploy(
+        "Voting Escrow Boost Delegation",
+        "veBoost",
+        "",
+        veOcean.address,
+        {"from": alice},
+    )
+
+    ocean.transfer(alice, TA, {"from": accounts[0]})
+    ocean.transfer(bob, TA, {"from": accounts[0]})
