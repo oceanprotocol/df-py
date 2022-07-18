90edb77ff62bffd5aa7b8c40f83b85d9c78b74ed
trizin <25263018+trizin@users.noreply.github.com>
2022-07-18 09:56:52 +0000

    Update veAllocate

--- contracts/ve/veAllocate.sol
+++ contracts/ve/veAllocate.sol
@@ -5,16 +5,12 @@ contract veAllocate {
     mapping(address => uint256) allocationCounter;
     mapping(address => mapping(uint256 => string)) allocationToId;
 
-    event AllocationAdded(
+    event AllocationSet(
         address indexed sender,
         string indexed id,
         uint256 amount
     );
-    event AllocationRemoved(
-        address indexed sender,
-        string indexed id,
-        uint256 amount
-    );
+    event AllocationRemoved(address indexed sender, string indexed id);
 
     function getveAllocation(address _address, string calldata _id)
         public
@@ -26,23 +22,27 @@ contract veAllocate {
         return veAllocation[_address][_id];
     }
 
-    function allocate(uint256 amount, string calldata _id) external {
+    function setAllocation(uint256 amount, string calldata _id) external {
         if (veAllocation[msg.sender][_id] == 0) {
             allocationToId[msg.sender][allocationCounter[msg.sender]] = _id;
             allocationCounter[msg.sender]++;
         } else {
-            require(veAllocation[msg.sender][_id] + amount <= 1000, "SM");
+            require(amount <= 1000, "SM");
         }
-        veAllocation[msg.sender][_id] = amount;
 
-        emit AllocationAdded(msg.sender, _id, amount);
+        if (amount == 0) {
+            _removeAllocation(_id);
+        } else {
+            veAllocation[msg.sender][_id] = amount;
+            emit AllocationSet(msg.sender, _id, amount);
+        }
     }
 
-    function removeAllocation(uint256 amount, string calldata _id) external {
-        require(veAllocation[msg.sender][_id] >= amount, "SM");
-        veAllocation[msg.sender][_id] -= amount;
+    function _removeAllocation(string calldata _id) internal {
+        require(veAllocation[msg.sender][_id] > 0, "SM");
+        veAllocation[msg.sender][_id] = 0;
 
-        emit AllocationRemoved(msg.sender, _id, amount);
+        emit AllocationRemoved(msg.sender, _id);
     }
 
     function totalAllocation(address _address)
