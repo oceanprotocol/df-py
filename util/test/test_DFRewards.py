714eb51553df6f19d0c8f266bbbe4113609ad8b5
trizin <25263018+trizin@users.noreply.github.com>
2022-07-26 07:19:02 +0000

    Remove tests

--- util/test/test_DFRewards.py
+++ util/test/test_DFRewards.py
@@ -215,66 +215,6 @@ def test_strategies():
 
 
 @enforce_types
-def test_claim_and_restake():
-    address_file = networkutil.chainIdToAddressFile(networkutil.DEV_CHAINID)
-    oceanutil.recordDeployedContracts(address_file)
-    OCEAN = oceanutil.OCEANtoken()
-
-    df_rewards = B.DFRewards.deploy({"from": accounts[0]})
-    df_strategy = B.DFStrategyV1.deploy(df_rewards.address, {"from": accounts[0]})
-    df_rewards.addStrategy(df_strategy.address)
-
-    tos = [a1]
-    values = [toBase18(50.0)]
-    OCEAN.approve(df_rewards, sum(values), {"from": accounts[0]})
-    df_rewards.allocate(tos, values, OCEAN.address, {"from": accounts[0]})
-
-    assert df_rewards.claimable(a1, OCEAN.address) == toBase18(50.0)
-
-    pools = []
-    amounts = []
-    POOL_COUNT = 10
-
-    for _ in range(POOL_COUNT):
-        (_, pool) = oceantestutil.randomDeployPool(accounts[0], OCEAN)
-        pools.append(pool)
-        amounts.append(toBase18(50 / POOL_COUNT) - 1)
-
-    for pool in pools:
-        assert pool.balanceOf(accounts[1]) == 0
-
-    with brownie.reverts("Not enough rewards"):
-        # Cannot claim what you don't have
-        df_strategy.claimAndStake(
-            OCEAN.address,
-            [pools[0].address],
-            [toBase18(100.0)],
-            {"from": accounts[1]},
-        )
-    with brownie.reverts("Lengths must match"):
-        # Cannot claim what you don't have
-        df_strategy.claimAndStake(
-            OCEAN.address,
-            [pools[0].address],
-            [5, 5, 5],
-            {"from": accounts[1]},
-        )
-
-    print(amounts)
-
-    df_strategy.claimAndStake(
-        OCEAN.address,
-        [pool.address for pool in pools],
-        amounts,
-        {"from": accounts[1]},
-    )
-
-    for pool in pools:
-        assert pool.balanceOf(accounts[1]) > 0
-    assert df_rewards.claimable(a1, OCEAN.address) == 0
-
-
-@enforce_types
 def _deployTOK(account):
     return B.Simpletoken.deploy("TOK", "TOK", 18, toBase18(100.0), {"from": account})
 
