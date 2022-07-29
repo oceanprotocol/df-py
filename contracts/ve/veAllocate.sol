ed08fae976fa263e988ee08f96c54d6fa647cb13
trizin <25263018+trizin@users.noreply.github.com>
2022-07-29 10:04:06 +0000

    Minimal veAllocate contract

--- contracts/ve/veAllocate.sol
+++ contracts/ve/veAllocate.sol
@@ -2,14 +2,11 @@ pragma solidity ^0.8.12;
 
 contract veAllocate {
     mapping(address => mapping(bytes32 => uint256)) veAllocation;
-    mapping(address => uint256) allocationCounter;
-    mapping(address => mapping(uint256 => bytes32)) allocationToId;
-    mapping(address => mapping(bytes32 => uint256)) idToAllocation;
     mapping(address => uint256) _totalAllocation;
 
     event AllocationSet(
         address indexed sender,
-        address indexed addr,
+        address indexed nft,
         uint256 chainId,
         uint256 amount,
         bytes32 id
@@ -35,20 +32,21 @@ contract veAllocate {
         return _totalAllocation[_address];
     }
 
+    function getId(address nft, uint256 chainId) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(nft, chainId));
+    }
+
     function setAllocation(
         uint256 amount,
-        address addr,
+        address nft,
         uint256 chainId
     ) external {
-        bytes32 _id = keccak256(abi.encodePacked(addr, "-", chainId));
+        bytes32 _id = getId(nft, chainId);
 
         require(amount <= 1000, "BM");
 
         if (veAllocation[msg.sender][_id] == 0) {
             require(amount > 0, "SM");
-            allocationToId[msg.sender][allocationCounter[msg.sender]] = _id;
-            idToAllocation[msg.sender][_id] = allocationCounter[msg.sender];
-            allocationCounter[msg.sender]++;
         }
 
         _totalAllocation[msg.sender] =
@@ -57,38 +55,6 @@ contract veAllocate {
             veAllocation[msg.sender][_id];
 
         veAllocation[msg.sender][_id] = amount;
-        emit AllocationSet(msg.sender, addr, chainId, amount, _id);
-    }
-
-    function getTotalAllocation(
-        address _address,
-        uint256 limit,
-        uint256 skip
-    )
-        external
-        view
-        returns (
-            bytes32[] memory allocationIds,
-            uint256[] memory allocationAmounts
-        )
-    {
-        // array of bytes32
-        allocationIds = new bytes32[](allocationCounter[_address]);
-
-        allocationAmounts = new uint256[](allocationCounter[_address]);
-
-        uint256 _limit = 0;
-        if (allocationCounter[_address] > limit + skip) {
-            _limit = limit;
-        } else {
-            _limit = allocationCounter[_address] - skip;
-        }
-
-        for (uint256 i = skip; i < skip + _limit; i++) {
-            allocationIds[i] = allocationToId[_address][i];
-            allocationAmounts[i] = veAllocation[_address][
-                allocationToId[_address][i]
-            ];
-        }
+        emit AllocationSet(msg.sender, nft, chainId, amount, _id);
     }
 }
