ff1a856be88833df10b4f5a676897ce5d5d7a19d
Berkay Saglam <25263018+trizin@users.noreply.github.com>
2022-07-31 13:15:59 +0300

    Simplify veAllocate (#239)
    * Simplify
    
    * Update tests
    
    * Update tests
    
    * Remove comments

--- util/test/veOcean/test_allocate_dt.py
+++ util/test/veOcean/test_allocate_dt.py
@@ -16,35 +16,28 @@ def test_get_total_allocation():
     nftaddr3 = accounts[2].address
 
     veAllocate.setAllocation(100, nftaddr1, 1, {"from": accounts[0]})
-    id = veAllocate.getId(nftaddr1, 1)
-    assert veAllocate.getveAllocation(accounts[0], id) == 100
+    assert veAllocate.getveAllocation(accounts[0], nftaddr1, 1) == 100
 
     veAllocate.setAllocation(25, nftaddr2, 1, {"from": accounts[0]})
-    id = veAllocate.getId(nftaddr2, 1)
-    assert veAllocate.getveAllocation(accounts[0], id) == 25
+    assert veAllocate.getveAllocation(accounts[0], nftaddr2, 1) == 25
 
     veAllocate.setAllocation(50, nftaddr3, 1, {"from": accounts[0]})
-    id = veAllocate.getId(nftaddr3, 1)
-    assert veAllocate.getveAllocation(accounts[0], id) == 50
+    assert veAllocate.getveAllocation(accounts[0], nftaddr3, 1) == 50
 
     veAllocate.setAllocation(0, nftaddr2, 1, {"from": accounts[0]})
-    id = veAllocate.getId(nftaddr2, 1)
-    assert veAllocate.getveAllocation(accounts[0], id) == 0
+    assert veAllocate.getveAllocation(accounts[0], nftaddr2, 1) == 0
 
 
 @enforce_types
 def test_events():
     nftaddr1 = accounts[1].address
     tx = veAllocate.setAllocation(100, nftaddr1, 1, {"from": accounts[0]})
-    assert tx.events["AllocationSet"].values()[:4] == [
+    assert tx.events["AllocationSet"].values() == [
         accounts[0].address,
         accounts[1].address,
         1,
         100,
     ]
-    assert (tx.events["AllocationSet"].values()[4]).hex() == veAllocate.getId(
-        nftaddr1, 1
-    ).hex()
 
 
 @enforce_types
