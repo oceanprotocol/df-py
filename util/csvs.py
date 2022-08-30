a20d050a68d3e59b09970e55d294174f9fdf6457
trizin <25263018+trizin@users.noreply.github.com>
2022-08-30 11:27:53 +0000

    Add `saveNftInfoCsv`

--- util/csvs.py
+++ util/csvs.py
@@ -6,6 +6,7 @@ from typing import Any, Dict, List
 from enforce_typing import enforce_types
 
 from util import constants, oceanutil
+from util.query import DataNFT
 
 
 # ========================================================================
@@ -155,6 +156,48 @@ def veOCEANCsvFilename(csv_dir: str) -> str:
     """Returns the veOCEAN filename"""
     return os.path.join(csv_dir, f"vebals.csv")
 
+
+# ========================================================================
+# nftinfo csv
+
+
+@enforce_types
+def saveNftInfoCsv(nftinfo: List[DataNFT], csv_dir: str, chainID: int):
+    """
+    @description
+      Save  the nftinfo for this chain. This csv is required for df-sql.
+
+    @arguments
+        nftinfo -- list of DataNFT
+        csv_dir -- directory that holds csv files
+        chainID -- chainID
+    """
+
+    assert os.path.exists(csv_dir), csv_dir
+    csv_file = nftInfoCsvFilename(csv_dir, chainID)
+    assert not os.path.exists(csv_file), csv_file
+
+    with open(csv_file, "w") as f:
+        writer = csv.writer(f)
+        row = ["chainID", "nft_addr", "did", "symbol", "basetoken_addr", "volume"]
+        writer.writerow(row)
+
+        for nft in nftinfo:
+            row = [
+                chainID,
+                nft.nft_addr.lower(),
+                nft.did,
+                nft.symbol,
+                nft.basetoken_addr.lower(),
+                nft.volume,
+            ]
+            writer.writerow(row)
+
+
+@enforce_types
+def nftInfoCsvFilename(csv_dir: str, chainID: int) -> str:
+    """Returns the nftinfo filename"""
+    return os.path.join(csv_dir, f"nftinfo_{chainID}.csv")
 
 
 # ========================================================================
