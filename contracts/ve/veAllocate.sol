284c4b72bc7f946524a064da578f86915969a5c6
alexcos20 <alex.coseru@gmail.com>
2022-09-03 16:48:46 +0300

    lint

--- contracts/ve/veAllocate.sol
+++ contracts/ve/veAllocate.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.12;
 
 contract veAllocate {
     mapping(address => mapping(address => mapping(uint256 => uint256)))
-         private veAllocation;
+        private veAllocation;
     mapping(address => uint256) private _totalAllocation;
 
     event AllocationSet(
