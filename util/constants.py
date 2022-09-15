492278b4eedd26f899fdcc035027d3b11534104b
trizin <25263018+trizin@users.noreply.github.com>
2022-09-15 09:42:46 +0000

    Remove trailing /

--- util/constants.py
+++ util/constants.py
@@ -3,7 +3,7 @@ import brownie
 ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
 
 MARKET_ASSET_BASE_URL = "https://market.oceanprotocol.com/asset/"
-AQUARIUS_BASE_URL = "https://v4.aquarius.oceanprotocol.com/"
+AQUARIUS_BASE_URL = "https://v4.aquarius.oceanprotocol.com"
 
 BROWNIE_PROJECT = brownie.project.load("./", name="MyProject")
 
