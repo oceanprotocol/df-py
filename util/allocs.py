eb6c33b8f40b398af4e39b76f47f6830708d160f
trentmc <gtrent@gmail.com>
2022-10-25 16:57:14 -0500

    Fix #347: Refactor calcrewards, for simplicity

---
+++ util/allocs.py
@@ -0,0 +1,32 @@
+from enforce_typing import enforce_types
+
+from util import cleancase
+
+
+@enforce_types
+def allocsToStakes(allocs: dict, vebals: dict) -> dict:
+    """
+    For each % allocated value, multiply it with appropriate balance and return
+    the absolute allocated value (="stake"). 
+
+    @arguments
+      allocs - dict of [chainID][nft_addr][LP_addr] : percent_allocation_float
+      vebals - dict of [LP_addr] : veOCEAN_float -- total balance per LP
+
+    @return 
+      stakes - dict of [chainID][nft_addr][LP_addr] : veOCEAN_float - abs alloc
+    """
+
+    allocs = cleancase.modAllocations(allocs)
+    vebals = cleancase.modVebals(vebals)
+
+    stakes = allocs.copy() #we'll be changing the values
+    for chainID in allocs:
+        for nft_addr in allocs[chainID]:
+            for LP_addr, perc_alloc in allocs[chainID][nft_addr].items():
+                vebal = vebals.get(LP_addr, 0.0)
+                stake = perc_alloc * vebal
+                stakes[chainID][nft_addr][LP_addr] = stake
+
+    cleancase.assertStakes(stakes)
+    return stakes
