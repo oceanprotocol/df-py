14104a51abd73cab855898b8309f0aa9175aab9e
trentmc <gtrent@gmail.com>
2022-10-27 12:04:13 -0500

    filter out negligible values

--- util/calcrewards.py
+++ util/calcrewards.py
@@ -119,6 +119,13 @@ def _calcRewardsUsd(S, V_USD, rewards_avail: float) -> np.ndarray:
                 stake_ij * TARGET_WPY,
             )
 
+    # filter negligible values (<0.00001% of total RF)
+    sum0 = np.sum(R[R < 0.000001])
+    R[R < 0.000001] = 0.0
+
+    if np.sum(R) == 0.0:
+        return np.zeros((N_i, N_j), dtype=float)
+            
     # postcondition: nans
     assert not np.isnan(np.min(R)), R
 
