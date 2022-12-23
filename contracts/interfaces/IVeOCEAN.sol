daf662ea09b5ea656b86eaad9a915ba8ced72c48
trentmc <gtrent@gmail.com>
2022-12-23 14:20:40 -0300

    wip

--- contracts/interfaces/IVeOCEAN.sol
+++ contracts/interfaces/IVeOCEAN.sol
@@ -1,5 +1,10 @@
 // SPDX-License-Identifier: (Apache-2.0 AND CC-BY-4.0)
-pragma solidity >=0.8.12;
+pragma solidity ^0.8.12;
+
+interface IVeOCEAN {
+    function deposit_for(address _address, uint256 _amount) external;
+    function create_lock(uint256 value, uint256 unlock_time) external;
+}
 
 interface IDFRewards {
     event Allocated(address[] tos, uint256[] values, address tokenAddress);
