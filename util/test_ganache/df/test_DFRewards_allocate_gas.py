3eb75a31b32b6960af2a364853387f7d7ebc68e5
trentmc <gtrent@gmail.com>
2022-12-26 15:12:58 -0300

    work under ocean lib regime. Tests are failing due to bad gas numbers. But this is because I have barge running on a newer fork

--- util/test_ganache/df/test_DFRewards_allocate_gas.py
+++ util/test_ganache/df/test_DFRewards_allocate_gas.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 import brownie
+from brownie.network import accounts
 import pytest
 from enforce_typing import enforce_types
 from ocean_lib.web3_internal.utils import connect_to_network
@@ -11,11 +12,11 @@ from util.constants import BROWNIE_PROJECT as B
 
 
 @enforce_types
-def test_allocate_gas():
-    one = _batch_allocate(1)
-    two = _batch_allocate(2)
-    nine = _batch_allocate(9)
-    ten = _batch_allocate(10)
+def test_allocate_gas(OCEAN, df_rewards):
+    one = _batch_allocate(1, OCEAN, df_rewards)
+    two = _batch_allocate(2, OCEAN, df_rewards)
+    nine = _batch_allocate(9, OCEAN, df_rewards)
+    ten = _batch_allocate(10, OCEAN, df_rewards)
 
     per_iteration1 = two.gas_used - one.gas_used
     per_iteration2 = ten.gas_used - nine.gas_used
@@ -31,38 +32,23 @@ def test_allocate_gas():
 
 
 @enforce_types
-def test_1250_addresses():
-    big_batch = _batch_allocate(1250)
+def test_1250_addresses(OCEAN, df_rewards):
+    big_batch = _batch_allocate(1250, OCEAN, df_rewards)
 
     # should be able to allocate 1250 addresses using less than 30,000,000 gas
     assert big_batch.gas_used < 30_000_000
 
 
 @enforce_types
-def test_insufficient_gas_reverts():
-    account0 = brownie.network.accounts[0]
-    addresses, rewards, token_addr, df_rewards = _prep_batch_allocate(1250)
-    with pytest.raises(Exception) as e_info:
-        df_rewards.allocate(
-            addresses, rewards, token_addr, {"from": account0, "gas_limit": 100000}
-        )
-    assert str(e_info.value) == "base fee exceeds gas limit"
-
-
-@enforce_types
-def _batch_allocate(number: int) -> str:
-    account0 = brownie.network.accounts[0]
-    addresses, rewards, token_addr, df_rewards = _prep_batch_allocate(number)
-    tx = df_rewards.allocate(addresses, rewards, token_addr, {"from": account0})
+def _batch_allocate(number: int, OCEAN, df_rewards) -> str:
+    addresses, rewards = _prep_batch_allocate(number, OCEAN, df_rewards)
+    tx = df_rewards.allocate(addresses, rewards, OCEAN.address, {"from": accounts[0]})
     return tx
 
 
 @enforce_types
-def _prep_batch_allocate(number: int) -> Any:
-    account0 = brownie.network.accounts[0]
-    OCEAN = oceanutil.OCEANtoken()
-    df_rewards = B.DFRewards.deploy({"from": account0})
+def _prep_batch_allocate(number: int, OCEAN, df_rewards) -> Any:
     addresses = get_random_addresses(number)
-    rewards = [1 for i in range(number)]
-    OCEAN.approve(df_rewards, sum(rewards), {"from": account0})
-    return addresses, rewards, OCEAN.address, df_rewards
+    rewards = [1] * number
+    OCEAN.approve(df_rewards.address, sum(rewards), {"from": accounts[0]})
+    return addresses, rewards
