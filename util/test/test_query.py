8e43b244d38e85a2340009585ef5ea208096754c
trizin <25263018+trizin@users.noreply.github.com>
2022-12-28 12:49:50 +0000

    Update tests

--- util/test/test_query.py
+++ util/test/test_query.py
@@ -548,32 +548,9 @@ testfunc_callcount = 0
 
 
 @enforce_types
-def test_retryFunction():
+def test_retryFunction_query():
     # pylint: disable=global-variable-undefined
     global testfunc_callcount
-    testfunc_callcount = 0
-
-    def testfunc_fail(some_arg: int):
-        # pylint: disable=global-variable-undefined
-        global testfunc_callcount
-        testfunc_callcount += 1
-        if testfunc_callcount == 3:
-            return testfunc_callcount + some_arg
-        raise Exception("failed")
-
-    some_arg = 1
-    assert (
-        query.retryFunction(testfunc_fail, 3, 0.1, some_arg)
-        == testfunc_callcount + some_arg
-    )
-    testfunc_callcount = 0
-
-    with raises(Exception):
-        query.retryFunction(testfunc_fail, 2, 0.1, some_arg)
-    testfunc_callcount = 0
-
-    with raises(Exception):
-        query.retryFunction(testfunc_fail, 1, 0.1, some_arg)
     testfunc_callcount = 0
 
     def testquery_fail():
