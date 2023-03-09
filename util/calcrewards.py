c5356ce3d8f6739257664c893f9e81ceeb8c1655
trizin <25263018+trizin@users.noreply.github.com>
2023-03-09 11:53:21 +0300

    Use namedtuple

--- util/calcrewards.py
+++ util/calcrewards.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timedelta
-from typing import Dict, List, Tuple, Union
-
+from typing import Dict, List, Tuple, Union, NamedTuple
 from enforce_typing import enforce_types
 import numpy as np
 import scipy
@@ -12,35 +11,39 @@ from util.constants import MAX_N_RANK_ASSETS, RANK_SCALE_OP
 # Weekly Percent Yield needs to be 1.5717%., for max APY of 125%
 TARGET_WPY = 0.015717
 
+class Period(NamedTuple):
+    start: datetime
+    supply: float
+    reward: float
+
 
 @enforce_types
 def getRewardAmount(start_dt: datetime) -> int:
     end_dt = start_dt + timedelta(days=7)
 
-    def getrew(period):
-        period_start = period["start"]
+    def getrew(period: Period) -> float:
+        period_start = period.start
         end_ts = (end_dt - period_start).total_seconds()
         start_ts = (start_dt - period_start).total_seconds()
-        return _halflife(period["supply"], end_ts, HALF_LIFE) - _halflife(
-            period["supply"], start_ts, HALF_LIFE
+        return _halflife(period.supply, end_ts, HALF_LIFE) - _halflife(
+            period.supply, start_ts, HALF_LIFE
         )
 
     TOT_SUPPLY = 503370000 * 1e18
     HALF_LIFE = 4 * 365 * 24 * 60 * 60  # 4 years
 
-    periods = [
-        {"start": datetime(2023, 3, 16), "supply": TOT_SUPPLY * 0.1, "reward": 0},
-        {"start": datetime(2024, 3, 15), "supply": TOT_SUPPLY * 0.15, "reward": 0},
-        {"start": datetime(2024, 9, 15), "supply": TOT_SUPPLY * 0.25, "reward": 0},
-        {"start": datetime(2025, 3, 15), "supply": TOT_SUPPLY * 0.5, "reward": 0},
+    periods: List[Period] = [
+        Period(start=datetime(2023, 3, 16), supply=TOT_SUPPLY * 0.1, reward=0),
+        Period(start=datetime(2024, 3, 15), supply=TOT_SUPPLY * 0.15, reward=0),
+        Period(start=datetime(2024, 9, 15), supply=TOT_SUPPLY * 0.25, reward=0),
+        Period(start=datetime(2025, 3, 15), supply=TOT_SUPPLY * 0.5, reward=0),
     ]
 
     for period in periods:
-        if start_dt >= period["start"]:
-            period["reward"] = getrew(period)
+        if start_dt >= period.start:
+            period.reward = getrew(period)
 
-    return int(sum(period["reward"] for period in periods))
-
+    return int(sum(period.reward for period in periods)))
 
 @enforce_types
 def _halflife(value, t, h) -> float:
