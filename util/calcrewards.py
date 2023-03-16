6fc1284b0ee9810379a2a1428e171e8a77817f1d
trizin <25263018+trizin@users.noreply.github.com>
2023-03-16 15:51:14 +0300

    Move vesting related things to its own module

--- util/calcrewards.py
+++ util/calcrewards.py
@@ -10,75 +10,10 @@ from util.base18 import fromBase18, toBase18
 from util.constants import (
     MAX_N_RANK_ASSETS,
     RANK_SCALE_OP,
-    ACTIVE_REWARDS_MULTIPLIER,
-    DFMAIN_CONSTANTS,
 )
 
 # Weekly Percent Yield needs to be 1.5717%., for max APY of 125%
 TARGET_WPY = 0.015717
-
-
-def getActiveRewardAmountForWeekEth(start_dt: datetime) -> int:
-    """
-    Return the reward amount for the week in ETH starting at start_dt.
-    This is the amount that will be allocated to active rewards.
-    """
-    total_reward_amount = getRewardAmountForWeekWei(start_dt)
-    active_reward_amount = int(total_reward_amount * ACTIVE_REWARDS_MULTIPLIER)
-    active_reward_amount_eth = fromBase18(active_reward_amount)
-    return active_reward_amount_eth
-
-
-@enforce_types
-def getRewardAmountForWeekWei(start_dt: datetime) -> int:
-    """
-    Return the total reward amount for the week in WEI starting at start_dt.
-    This amount is in accordance with the vesting schedule.
-    Returns 0 if the week is before the start of the vesting schedule (DF29).
-    """
-
-    # hardcoded values for linear vesting schedule
-    dfweek = getDfWeekNumber(start_dt) - 1
-
-    for start_week, value in DFMAIN_CONSTANTS.items():
-        if dfweek < start_week:
-            return toBase18(value)
-
-    # halflife
-    TOT_SUPPLY = 503370000 * 1e18
-    HALF_LIFE = 4 * 365 * 24 * 60 * 60  # 4 years
-    end_dt = start_dt + timedelta(days=7)
-
-    vesting_start_dt = datetime(2025, 3, 13)
-    vesting_tot_amount = TOT_SUPPLY - 32530000
-    reward = _halflife_solidity(
-        vesting_tot_amount, (end_dt - vesting_start_dt).total_seconds(), HALF_LIFE
-    ) - _halflife_solidity(
-        vesting_tot_amount, (start_dt - vesting_start_dt).total_seconds(), HALF_LIFE
-    )
-    return int(reward)
-
-
-@enforce_types
-def _halflife(value, t, h) -> int:
-    """
-    Approximation of halflife function
-    """
-    t = int(t)
-    h = int(h)
-    value = int(value)
-    p = value >> int(t // h)
-    t %= h
-    return int(value - p + (p * t) // h // 2)
-
-
-@enforce_types
-def _halflife_solidity(value, t, h) -> int:
-    """
-    Halflife function in Solidity, requires network connection and
-    deployed VestingWallet contract
-    """
-    return oceanutil.VestingWalletV0().getAmount(value, t, h)
 
 
 @enforce_types
--- util/test/test_calcrewards.py
+++ util/test/test_calcrewards.py
@@ -811,68 +811,6 @@ def test_flattenRewards():
     }
 
 
-test_params = [
-    (datetime(2023, 3, 9), 0),
-    (datetime(2023, 3, 16), 150000.0),
-    (datetime(2023, 4, 13), 150000.0),
-    (datetime(2023, 6, 15), 150000.0),
-    (datetime(2024, 3, 7), 150000.0),
-    (datetime(2024, 3, 14), 300000),
-    (datetime(2024, 9, 5), 300000),
-    (datetime(2024, 9, 12), 600000),
-    (datetime(2025, 3, 6), 600000),
-    (datetime(2025, 3, 13), 1206708.9),
-    (datetime(2025, 3, 20), 1206708.9),
-    (datetime(2029, 2, 22), 1206708.9),
-    (datetime(2029, 3, 1), 1206708.9),
-    (datetime(2029, 3, 8), 948128.42),
-    (datetime(2029, 3, 15), 603354.452),
-    (datetime(2033, 2, 24), 603354.452),
-    (datetime(2033, 3, 3), 603354.452),
-    (datetime(2033, 3, 10), 344773.972),
-    (datetime(2033, 3, 17), 301677.22),
-    (datetime(2037, 1, 14), 301677.22),
-    (datetime(2037, 2, 26), 301677.226),
-    (datetime(2037, 3, 5), 258580.47),
-    (datetime(2037, 3, 12), 150838.61),
-    (datetime(2041, 2, 21), 150838.61),
-    (datetime(2041, 2, 28), 150838.61),
-    (datetime(2041, 3, 7), 96967.67),
-    (datetime(2041, 3, 14), 75419.30),
-    (datetime(2041, 3, 14), 75419.30),
-    (datetime(2045, 3, 14), 37709.65),
-    # may df-py live long and prosper
-]
-
-
-@pytest.mark.parametrize("test_input, expected_output", test_params)
-def test_getRewardAmountForWeekWei(test_input, expected_output):
-    networkutil.connect(networkutil.DEV_CHAINID)
-    assert fromBase18(calcrewards.getRewardAmountForWeekWei(test_input)) == approx(
-        expected_output
-    )
-
-
-@pytest.mark.parametrize("test_input, expected_output", test_params)
-def test_getActiveRewardAmountForWeekEth(test_input, expected_output):
-    networkutil.connect(networkutil.DEV_CHAINID)
-    assert calcrewards.getActiveRewardAmountForWeekEth(test_input) == approx(
-        expected_output * ACTIVE_REWARDS_MULTIPLIER
-    )
-
-
-@enforce_types
-def test_compareHalflifeFunctions():
-    # compare python and solidity halflife function
-    VALUE = 503370000 * 1e18
-    HALF_LIFE = 4 * 365 * 24 * 60 * 60  # 4 years
-    i = HALF_LIFE
-    py_result = calcrewards._halflife(VALUE, i, HALF_LIFE)
-    solidity_result = calcrewards._halflife_solidity(VALUE, i, HALF_LIFE)
-    diff = abs(py_result - solidity_result)
-    assert diff < 1e10, f"diff {diff} at i {i/HALF_LIFE*2}"
-
-
 # ========================================================================
 # Helpers to keep function calls compact, and return vals compact.
 
