aa63bed3dc765147190a937b18bd6b676fd80e0c
trizin <25263018+trizin@users.noreply.github.com>
2023-04-05 20:56:56 +0300

    Ensure gas costs are not duplicated (#525)

--- util/query.py
+++ util/query.py
@@ -417,6 +417,7 @@ def _queryVolsOwners(
     vols: Dict[str, Dict[str, float]] = {}
     gasvols: Dict[str, Dict[str, float]] = {}
     owners: Dict[str, float] = {}
+    txgascost: Dict[str, float] = {}  # tx hash : gas cost
 
     chunk_size = 1000  # max for subgraph = 1000
     offset = 0
@@ -444,7 +445,8 @@ def _queryVolsOwners(
             lastPriceValue,
             block,
             gasPrice,
-            gasUsed
+            gasUsed,
+            tx
           }
         }
         """ % (
@@ -487,7 +489,9 @@ def _queryVolsOwners(
                 if nft_addr not in gasvols[native_token_addr]:
                     gasvols[native_token_addr][nft_addr] = 0
 
-                gasvols[native_token_addr][nft_addr] += gasCost
+                if order["tx"] not in txgascost:
+                    txgascost[order["tx"]] = gasCost
+                    gasvols[native_token_addr][nft_addr] += gasCost
 
             if lastPriceValue == 0:
                 continue
