114ea1c6eecf5b78b9aafc0100e7e8daf5c8cbe8
Calina Cenan <calina@cenan.net>
2023-05-01 13:51:48 +0000

    More tests.

--- util/test/test_judge.py
+++ util/test/test_judge.py
@@ -1,8 +1,11 @@
+import os
 from datetime import datetime, timedelta
+from unittest.mock import Mock, patch
+
+from predict_eth.helpers import create_alice_wallet, create_ocean_instance
 from requests.models import Response
 
-from util.judge import get_nft_addresses
-from unittest.mock import Mock, patch
+from util.judge import get_nft_addresses, nft_addr_to_pred_vals
 
 
 def test_get_nft_addresses():
@@ -35,3 +38,17 @@ def test_get_nft_addresses():
     assert "0x1234" in nft_addresses
     assert "0x456" not in nft_addresses
     assert "0x789" not in nft_addresses
+
+
+def test_get_pred_vals():
+    ocean = create_ocean_instance("polygon-test")
+    alice_private_key = os.getenv("REMOTE_TEST_PRIVATE_KEY1")
+    assert alice_private_key, "need envvar REMOTE_TEST_PRIVATE_KEY1"
+    alice = create_alice_wallet(ocean)  # uses REMOTE_TEST_PRIVATE_KEY1
+
+    pred_vals = nft_addr_to_pred_vals(
+        "0x471817de04faa9b616ed7644117d957439717bf9", ocean, alice
+    )
+
+    assert len(pred_vals) == 12
+    assert pred_vals[0] == 1633.1790360265798
