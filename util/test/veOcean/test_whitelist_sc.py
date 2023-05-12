fe871071eca4738a882354abe1484717b7f3ff31
Trent McConaghy <gtrent@gmail.com>
2023-05-12 15:02:47 +0900

    For #575, skip tests until fixed (PR #576)

--- util/test/veOcean/test_whitelist_sc.py
+++ util/test/veOcean/test_whitelist_sc.py
@@ -29,6 +29,7 @@ def test_velock_not_whitelisted():
 
 
 @enforce_types
+@pytest.mark.skip(reason="unskip once #575 fixed")
 def test_velock_whitelisted():
     """Test that a whitelisted contract can create a lock."""
 
