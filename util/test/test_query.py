e74c60d7f37c7820c057b54be8e74d871d559866
trizin <25263018+trizin@users.noreply.github.com>
2023-05-15 15:47:45 +0000

    Run querynftinfo at the end

--- util/test/test_query.py
+++ util/test/test_query.py
@@ -82,7 +82,8 @@ def test_all(tmp_path):
     print("Delegate...")
     ve_delegate(accounts[0], accounts[1], 0.5, 0)  # 0 -> 1 50%
     print(f"  {accounts[0].address} -> {accounts[1].address} 50%")
-    ve_delegate(accounts[0], zerobal_delegation_acct, 0.1, 1)  # 0 -> zerobal 5%
+    ve_delegate(accounts[0], zerobal_delegation_acct,
+                0.1, 1)  # 0 -> zerobal 5%
     print(f"  {accounts[0].address} -> {zerobal_delegation_acct.address} 10%")
     ve_delegate(accounts[3], accounts[4], 1.0, 0)  # 3 -> 4 100%
     print(f"  {accounts[3].address} -> {accounts[4].address} 100%")
@@ -138,11 +139,6 @@ def test_all(tmp_path):
     _test_queryAllocations(rng, sampling_accounts_addrs)
     _test_queryVolsOwnersSymbols(ST, FIN)
 
-    # FIX-ME
-    # Running this test causes the following error:
-    #   brownie.exceptions.ContractNotFound: This contract no longer exists.
-    # _test_queryNftinfo()
-
     # test dftool
     _test_dftool_query(tmp_path, ST, FIN)
     _test_dftool_nftinfo(tmp_path, FIN)
@@ -165,6 +161,10 @@ def test_all(tmp_path):
 
     # check balances again
     _test_queryVebalances(rng, sampling_accounts_addrs, delegation_accounts)
+
+    # Running this test before other tests causes the following error:
+    #   brownie.exceptions.ContractNotFound: This contract no longer exists.
+    _test_queryNftinfo()
 
 
 # =========================================================================
@@ -278,7 +278,8 @@ def _test_queryVolsOwners(st, fin):
 
     # test C0 (owners)
     assert C0, (V0, C0)
-    V0_nft_addrs = set(nft_addr for token_addr in V0 for nft_addr in V0[token_addr])
+    V0_nft_addrs = set(
+        nft_addr for token_addr in V0 for nft_addr in V0[token_addr])
     for C0_nft_addr in C0:
         assert C0_nft_addr in V0_nft_addrs
 
@@ -298,11 +299,12 @@ def _test_queryVolsOwnersSymbols(st, fin):
 @enforce_types
 def _test_queryNftinfo():
     print("_test_queryNftinfo()...")
-    nfts = query.queryNftinfo(CHAINID)
-    assert len(nfts) > 0
 
     nfts_block = query.queryNftinfo(137, 29778602)
     assert len(nfts_block) == 11
+
+    nfts = query.queryNftinfo(CHAINID)
+    assert len(nfts) > 0
 
     nfts_latest = query.queryNftinfo(CHAINID, "latest")
     assert len(nfts_latest) == len(nfts)
@@ -390,7 +392,8 @@ def _test_dftool_allocations(tmp_path, ST, FIN):
 
     # test result
     allocations_csv = csvs.allocationCsvFilename(CSV_DIR, False)
-    assert os.path.exists(allocations_csv), "allocations_realtime csv not found"
+    assert os.path.exists(
+        allocations_csv), "allocations_realtime csv not found"
 
 
 # =========================================================================
@@ -477,7 +480,8 @@ def _test_end_to_end_with_csvs(rng, tmp_path):
     rewardsperlp = csvs.loadRewardsCsv(csv_dir, "OCEAN")
     dfrewards_addr = B.DFRewards.deploy({"from": god_acct}).address
     OCEAN_addr = oceanutil.OCEAN_address()
-    dispense.dispense(rewardsperlp[CHAINID], dfrewards_addr, OCEAN_addr, god_acct)
+    dispense.dispense(rewardsperlp[CHAINID],
+                      dfrewards_addr, OCEAN_addr, god_acct)
 
 
 @enforce_types
@@ -549,7 +553,8 @@ def test_queryVebalances_empty():
 # pylint: disable=too-many-statements
 @enforce_types
 def test_allocation_sampling():
-    alice, bob, carol, karen, james = [brownie.accounts.add() for _ in range(5)]
+    alice, bob, carol, karen, james = [
+        brownie.accounts.add() for _ in range(5)]
     god_acct.transfer(alice, "1 ether")
     god_acct.transfer(bob, "1 ether")
     god_acct.transfer(carol, "1 ether")
@@ -573,23 +578,30 @@ def test_allocation_sampling():
     start_block = len(chain)
 
     # DAY 0
-    oceanutil.set_allocation(10000, allocate_addrs[0], CHAINID, alice)  # 100% at 0
-    oceanutil.set_allocation(10000, allocate_addrs[0], CHAINID, bob)  # 100% at 0
-    oceanutil.set_allocation(1000, allocate_addrs[0], CHAINID, karen)  # 10% at 0
-    oceanutil.set_allocation(1000, allocate_addrs[0], CHAINID, james)  # 10% at 0
+    oceanutil.set_allocation(
+        10000, allocate_addrs[0], CHAINID, alice)  # 100% at 0
+    oceanutil.set_allocation(
+        10000, allocate_addrs[0], CHAINID, bob)  # 100% at 0
+    oceanutil.set_allocation(
+        1000, allocate_addrs[0], CHAINID, karen)  # 10% at 0
+    oceanutil.set_allocation(
+        1000, allocate_addrs[0], CHAINID, james)  # 10% at 0
 
     forward(100)
 
     # DAY 1
     # Bob removes and re-adds 100%
     oceanutil.set_allocation(0, allocate_addrs[0], CHAINID, bob)  # 0% at 1
-    oceanutil.set_allocation(10000, allocate_addrs[1], CHAINID, bob)  # 100% at 1
+    oceanutil.set_allocation(
+        10000, allocate_addrs[1], CHAINID, bob)  # 100% at 1
 
     # Karen allocates 10%
-    oceanutil.set_allocation(1000, allocate_addrs[1], CHAINID, karen)  # 10% at 1
+    oceanutil.set_allocation(
+        1000, allocate_addrs[1], CHAINID, karen)  # 10% at 1
 
     # James allocates 20%
-    oceanutil.set_allocation(2000, allocate_addrs[0], CHAINID, james)  # 20% at 1
+    oceanutil.set_allocation(
+        2000, allocate_addrs[0], CHAINID, james)  # 20% at 1
 
     forward(100)
 
@@ -720,7 +732,8 @@ def test_symbol():
     testToken = B.Simpletoken.deploy("CO2", "", 18, 1e26, {"from": god_acct})
     assert query.symbol(testToken.address) == "CO2"
 
-    testToken = B.Simpletoken.deploy("ASDASDASD", "", 18, 1e26, {"from": god_acct})
+    testToken = B.Simpletoken.deploy(
+        "ASDASDASD", "", 18, 1e26, {"from": god_acct})
     assert query.symbol(testToken.address) == "ASDASDASD"
 
     testToken = B.Simpletoken.deploy(
