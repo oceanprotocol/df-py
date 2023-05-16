7026315ebce8fd3c656eb491c8f940136f207d7f
trizin <25263018+trizin@users.noreply.github.com>
2023-05-16 09:15:29 +0000

    Unskip tests

--- util/test/veOcean/test_whitelist_sc.py
+++ util/test/veOcean/test_whitelist_sc.py
@@ -29,7 +29,6 @@ def test_velock_not_whitelisted():
 
 
 @enforce_types
-@pytest.mark.skip(reason="Skip until #575 fixed")
 def test_velock_whitelisted():
     """Test that a whitelisted contract can create a lock."""
 
