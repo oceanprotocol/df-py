9cfb4b0c24c88792438ec7744de836ec6b0fdc65
trentmc <gtrent@gmail.com>
2023-05-17 23:26:02 +0900

    include pylint in requirements.txt, it should be for pylint 2.17.4 now (I'd been in 2.12 before). Fix pylint warnings based on my local change.

--- util/graphutil.py
+++ util/graphutil.py
@@ -5,7 +5,8 @@ from util import networkutil
 
 def submitQuery(query: str, chainID: int) -> dict:
     subgraph_url = networkutil.chainIdToSubgraphUri(chainID)
-    request = requests.post(subgraph_url, "", json={"query": query})
+    request = requests.post(
+        subgraph_url, "", json={"query": query}, timeout=30)
     if request.status_code != 200:
         # pylint: disable=broad-exception-raised
         raise Exception(f"Query failed. Return code is {request.status_code}\n{query}")
