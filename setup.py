1fac3320e3915fa6b9fd15668602e803af4b2954
Călina Cenan <calina@cenan.net>
2023-05-19 20:07:44 +0300

    Towards #597 - Df-py Versioning (#603)
    * Adds bumpversion to requirements.
    
    * Replace with bumpversion.cfg
    
    * Bump version: 0.0.0 → 0.0.1
    
    * Move to setup.py.
    
    * Bump version: 0.0.1 → 0.0.2
    
    * Add to bumpversion config.
    
    * Bump version: 0.0.2 → 0.0.3
    
    * Fix black and mypy.

---
+++ setup.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2023 Ocean Protocol Foundation
+# SPDX-License-Identifier: Apache-2.0
+#
+
+"""The setup script."""
+
+#  Copyright 2018 Ocean Protocol Foundation
+#  SPDX-License-Identifier: Apache-2.0
+
+from setuptools import find_packages, setup
+
+# Installed by pip install ocean-provider
+# or pip install -e .
+install_requirements = [
+    "coverage",
+    "ccxt==3.0.84",
+    "eciespy",
+    "eth-brownie",
+    "enforce_typing",
+    "gql",
+    "mypy",
+    "numpy",
+    "pandas",
+    "pylint",
+    "pytest",
+    "requests",
+    "scipy",
+    "types-requests",
+    "bumpversion",
+]
+
+# Required to run setup.py:
+setup_requirements = ["pytest-runner"]
+
+setup(
+    author="oceanprotocol",
+    author_email="devops@oceanprotocol.com",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.8",
+    ],
+    description="DF PY lib.",
+    install_requires=install_requirements,
+    license="Apache Software License 2.0",
+    include_package_data=True,
+    packages=find_packages(
+        include=[
+            "util",
+            "build",
+            "contracts",
+            "data",
+            "interfaces",
+            "reports",
+            "scripts",
+        ]
+    ),
+    setup_requires=setup_requirements,
+    test_suite="tests",
+    url="https://github.com/oceanprotocol/provider-py",
+    # fmt: off
+    # bumpversion needs single quotes
+    version='0.0.3',
+    # fmt: on
+    zip_safe=False,
+)
