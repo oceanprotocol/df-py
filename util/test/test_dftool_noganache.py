09b1b98459d48ec8cc918dd6a90960f37eadd97c
trentmc <gtrent@gmail.com>
2023-05-19 21:23:40 +0900

    fix test_dftool_noganache

--- util/test/test_dftool_noganache.py
+++ util/test/test_dftool_noganache.py
@@ -1,139 +1,32 @@
-import datetime
 import os
 import subprocess
+from typing import List
 
-import brownie
 from enforce_typing import enforce_types
+import pytest
 
-from util import csvs, networkutil, oceanutil, oceantestutil
-from util.base18 import from_wei, to_wei
-from util.constants import BROWNIE_PROJECT as B
-
-PREV, DFTOOL_ACCT = {}, None
-
-CHAINID = networkutil.DEV_CHAINID
-ADDRESS_FILE = networkutil.chainIdToAddressFile(CHAINID)
+from util import csvs, dftool_module
 
 
 @enforce_types
-def test_calc(tmp_path):
+def test_getrate(tmp_path):
+    TOKEN_SYMBOL = "OCEAN"
+    ST = "2022-01-01"
+    FIN = "2022-02-02"
     CSV_DIR = str(tmp_path)
-    OCEAN_addr = oceanutil.OCEAN_address()
 
-    # insert fake csvs
-    allocations = {CHAINID: {"0xnft_addra": {"0xlp_addr1": 1.0}}}
-    csvs.saveAllocationCsv(allocations, CSV_DIR)
-
-    nftvols_at_chain = {OCEAN_addr: {"0xnft_addra": 1.0}}
-    csvs.saveNftvolsCsv(nftvols_at_chain, CSV_DIR, CHAINID)
-
-    owners_at_chain = {"0xnft_addra": "0xlp_addr1"}
-    csvs.saveOwnersCsv(owners_at_chain, CSV_DIR, CHAINID)
-
-    vebals = {"0xlp_addr1": 1.0}
-    locked_amt = {"0xlp_addr1": 10.0}
-    unlock_time = {"0xlp_addr1": 1}
-    csvs.saveVebalsCsv(vebals, locked_amt, unlock_time, CSV_DIR)
-
-    symbols_at_chain = {OCEAN_addr: "OCEAN"}
-    csvs.saveSymbolsCsv(symbols_at_chain, CSV_DIR, CHAINID)
-
-    csvs.saveRateCsv("OCEAN", 0.50, CSV_DIR)
-
-    # main cmd
-    TOT_OCEAN = 1000.0
-    cmd = f"./dftool calc {CSV_DIR} {TOT_OCEAN}"
+    cmd = f"./dftool getrate {TOKEN_SYMBOL} {ST} {FIN} {CSV_DIR}"
     os.system(cmd)
 
     # test result
-    rewards_csv = csvs.rewardsperlpCsvFilename(CSV_DIR, "OCEAN")
-    assert os.path.exists(rewards_csv)
+    assert csvs.rateCsvFilenames(CSV_DIR)
 
 
 @enforce_types
-def test_calc_without_amount(tmp_path):
-    CSV_DIR = str(tmp_path)
-    OCEAN_addr = oceanutil.OCEAN_address()
-
-    # insert fake csvs
-    allocations = {CHAINID: {"0xnft_addra": {"0xlp_addr1": 1.0}}}
-    csvs.saveAllocationCsv(allocations, CSV_DIR)
-
-    nftvols_at_chain = {OCEAN_addr: {"0xnft_addra": 1e10}}
-    csvs.saveNftvolsCsv(nftvols_at_chain, CSV_DIR, CHAINID)
-
-    owners_at_chain = {"0xnft_addra": "0xlp_addr1"}
-    csvs.saveOwnersCsv(owners_at_chain, CSV_DIR, CHAINID)
-
-    vebals = {"0xlp_addr1": 1e8}
-    locked_amt = {"0xlp_addr1": 10.0}
-    unlock_time = {"0xlp_addr1": 1}
-    csvs.saveVebalsCsv(vebals, locked_amt, unlock_time, CSV_DIR)
-
-    symbols_at_chain = {OCEAN_addr: "OCEAN"}
-    csvs.saveSymbolsCsv(symbols_at_chain, CSV_DIR, CHAINID)
-
-    csvs.saveRateCsv("OCEAN", 0.50, CSV_DIR)
-
-    # main cmd
-    TOT_OCEAN = 0
-    ST = "2023-03-16"  # first week of df main
-    cmd = f"./dftool calc {CSV_DIR} {TOT_OCEAN} {ST}"
-    os.system(cmd)
-
-    # test result
-    rewards_csv = csvs.rewardsperlpCsvFilename(CSV_DIR, "OCEAN")
-    assert os.path.exists(rewards_csv)
-
-    # get total reward amount
-    rewards = csvs.loadRewardsCsv(CSV_DIR, "OCEAN")
-    total_reward = 0
-    for _, addrs in rewards.items():
-        for _, reward in addrs.items():
-            total_reward += reward
-    assert total_reward == 75000.0
-
-
-@enforce_types
-def test_dispense(tmp_path):
-    # values used for inputs or main cmd
-    accounts = brownie.network.accounts
-    address1 = accounts[1].address.lower()
-    address2 = accounts[2].address.lower()
-    CSV_DIR = str(tmp_path)
-    TOT_OCEAN = 1000.0
-
-    # accounts[0] has OCEAN. Ensure that ispensing account has some
-    global DFTOOL_ACCT
-    OCEAN = oceanutil.OCEANtoken()
-    OCEAN.transfer(DFTOOL_ACCT, to_wei(TOT_OCEAN), {"from": accounts[0]})
-    assert from_wei(OCEAN.balanceOf(DFTOOL_ACCT.address)) == TOT_OCEAN
-
-    # insert fake inputs: rewards csv, new dfrewards.sol contract
-    rewards = {
-        CHAINID: {address1: 400},
-        "5": {address1: 300, address2: 100},
-    }
-    csvs.saveRewardsperlpCsv(rewards, CSV_DIR, "OCEAN")
-
-    df_rewards = B.DFRewards.deploy({"from": accounts[0]})
-
-    # main command
-    CSV_DIR = str(tmp_path)
-    DFREWARDS_ADDR = df_rewards.address
-    OCEAN_ADDR = oceanutil.OCEAN_address()
-
-    cmd = f"./dftool dispense_active {CSV_DIR} {CHAINID} {DFREWARDS_ADDR} {OCEAN_ADDR}"
-    os.system(cmd)
-
-    # test result
-    assert from_wei(df_rewards.claimable(address1, OCEAN_ADDR)) == 700.0
-    assert from_wei(df_rewards.claimable(address2, OCEAN_ADDR)) == 100.0
-
-
-@enforce_types
-def test_manyrandom():
-    cmd = f"./dftool manyrandom {CHAINID}"
+@pytest.mark.skip(reason="Passing. However script executes N commands ~18m")
+def test_gen_hist_data():
+    os.environ["USE_TESTNET"] = "1"
+    cmd = "./scripts/gen_hist_data.sh 22 round_22"
     output_s = ""
     with subprocess.Popen(
         cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
@@ -145,123 +38,39 @@ def test_manyrandom():
 
 
 @enforce_types
-def test_checkpoint_feedistributor():
-    feeDistributor = oceanutil.FeeDistributor()
-    timecursor_before = feeDistributor.time_cursor()
-    brownie.network.chain.sleep(60 * 60 * 24 * 7)
-    brownie.network.chain.mine()
-    cmd = f"./dftool checkpoint_feedist {CHAINID}"
-    os.system(cmd)
+def test_noarg_commands():
+    # Test commands that have no args
+    subargs = _get_HELP_SHORT_subargs_in_dftool()  # key args only, for speed
+    subargs = [""] + ["badarg"] + subargs
+    for subarg in subargs:
+        print(f"CMD: dftool {subarg}")
+        cmd = f"./dftool {subarg}"
 
-    timecursor_after = feeDistributor.time_cursor()
+        output_s = ""
+        with subprocess.Popen(
+            cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+        ) as proc:
+            while proc.poll() is None:
+                output_s += proc.stdout.readline().decode("ascii")
 
-    assert timecursor_after > timecursor_before
-
-
-@enforce_types
-def test_calc_passive(tmp_path):
-    accounts = []
-    account0 = brownie.network.accounts[0]
-    OCEAN = oceanutil.OCEANtoken()
-    OCEAN_lock_amt = to_wei(10.0)
-    S_PER_WEEK = 604800
-    chain = brownie.network.chain
-    feeDistributor = oceanutil.FeeDistributor()
-    veOCEAN = oceanutil.veOCEAN()
-    CSV_DIR = str(tmp_path)
-    unlock_time = chain.time() + S_PER_WEEK * 10
-
-    for _ in range(2):
-        acc = brownie.network.accounts.add()
-        account0.transfer(acc, to_wei(0.1))
-        OCEAN.transfer(acc, OCEAN_lock_amt, {"from": account0})
-        # create lock
-        OCEAN.approve(veOCEAN, OCEAN_lock_amt, {"from": acc})
-        veOCEAN.create_lock(OCEAN_lock_amt, unlock_time, {"from": acc})
-        accounts.append(acc)
-
-    for _ in range(3):
-        OCEAN.transfer(
-            feeDistributor.address,
-            to_wei(1000.0),
-            {"from": brownie.accounts[0]},
-        )
-        chain.sleep(S_PER_WEEK)
-        chain.mine()
-        feeDistributor.checkpoint_token({"from": brownie.accounts[0]})
-        feeDistributor.checkpoint_total_supply({"from": brownie.accounts[0]})
-
-    fake_vebals = {}
-    locked_amt = {}
-    unlock_times = {}
-    for acc in accounts:
-        fake_vebals[acc.address] = from_wei(veOCEAN.balanceOf(acc.address))
-        locked_amt[acc.address] = OCEAN_lock_amt
-        unlock_times[acc.address] = unlock_time
-    csvs.saveVebalsCsv(fake_vebals, locked_amt, unlock_times, CSV_DIR, False)
-    date = chain.time() // S_PER_WEEK * S_PER_WEEK
-    date = datetime.datetime.utcfromtimestamp(date).strftime("%Y-%m-%d")
-    cmd = f"./dftool calculate_passive {CHAINID} {date} {CSV_DIR}"
-    os.system(cmd)
-
-    filename = csvs.passiveCsvFilename(CSV_DIR)
-    assert os.path.exists(filename)
-
-    # number of lines must be >=3
-    with open(filename, "r") as f:
-        lines = f.readlines()
-        assert len(lines) >= 3
-
-
-def test_initdevwallets():
-    account9 = brownie.network.accounts[9]
-
-    OCEAN = oceanutil.OCEANtoken()
-    if OCEAN.balanceOf(account9.address) == 0.0:
-        assert from_wei(OCEAN.balanceOf(account9.address)) == 0.0
-
-        cmd = f"./dftool initdevwallets {networkutil.DEV_CHAINID}"
-        os.system(cmd)
-
-        assert from_wei(OCEAN.balanceOf(account9.address)) > 1.0
+        return_code = proc.wait()
+        # bad commands - such as querymany - will still return 0 and do not fail
+        assert return_code == 0, f"'dftool {subarg}' failed. \n{output_s}"
 
 
 @enforce_types
-def setup_function():
-    global PREV, DFTOOL_ACCT
+def _get_HELP_SHORT_subargs_in_dftool() -> List[str]:
+    """Return e.g. ["help", "compile", "getrate", "volsym", ...]"""
+    s_lines = dftool_module.HELP_SHORT.split("\n")
 
-    networkutil.connect(CHAINID)
-    accounts = brownie.network.accounts
-    oceanutil.recordDevDeployedContracts()
-    oceantestutil.fillAccountsWithOCEAN()
+    subargs = []
+    for s_line in s_lines:
+        if "Usage:" in s_line:
+            continue
+        if "dftool " not in s_line:
+            continue
+        subarg = s_line.lstrip().split(" ")[1]  # e.g. "compile"
+        subargs.append(subarg)
 
-    DFTOOL_ACCT = accounts.add()
-    accounts[0].transfer(DFTOOL_ACCT, to_wei(0.001))
-
-    for envvar in [
-        "DFTOOL_KEY",
-        "ADDRESS_FILE",
-        "SUBGRAPH_URI",
-        "SECRET_SEED",
-        "WEB3_INFURA_PROJECT_ID",
-    ]:
-        PREV[envvar] = os.environ.get(envvar)
-
-    os.environ["DFTOOL_KEY"] = DFTOOL_ACCT.private_key
-    os.environ["ADDRESS_FILE"] = ADDRESS_FILE
-    os.environ["SUBGRAPH_URI"] = networkutil.chainIdToSubgraphUri(CHAINID)
-    os.environ["SECRET_SEED"] = "1234"
-    os.environ["WEB3_INFURA_PROJECT_ID"] = "9aa3d95b3bc440fa88ea12eaa4456161"
-
-
-@enforce_types
-def teardown_function():
-    networkutil.disconnect()
-
-    global PREV
-    for envvar, envval in PREV.items():
-        if envval is None:
-            del os.environ[envvar]
-        else:
-            os.environ[envvar] = envval
-    PREV = {}
+    assert "compile" in subargs  # postcondition
+    return subargs
