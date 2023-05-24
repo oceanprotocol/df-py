416d9177b54be37b258c1f12539637f824ba10e7
trizin <25263018+trizin@users.noreply.github.com>
2023-05-24 15:49:38 +0300

    Add predictoor models

---
+++ util/predictoor/models.py
@@ -0,0 +1,50 @@
+from enforce_typing import enforce_types
+from util.constants import MIN_PREDICTIONS
+
+
+class Prediction:
+
+    @enforce_types
+    def __init__(self, block: int, payout: float, contract_addr: str):
+        self.block = block
+        self.payout = payout
+        self.contract_addr = contract_addr
+
+    @property
+    def correct(self) -> bool:
+        """
+        Returns true if the prediction is correct, false otherwise.
+        """
+        # We assume that the prediction is wrong if the payout is 0.
+        # Only predictions where the true value for their slot is submitted are being counted, so this is a safe assumption.
+        return self.payout > 0
+
+
+class Predictoor:
+
+    @enforce_types
+    def __init__(self, address: str):
+        self.predictions = []
+        self.address = address
+
+    @enforce_types
+    def add_prediction(self, prediction: Prediction):
+        """
+        Adds a prediction to the list of predictions for this Predictoor.
+        @params
+            prediction (Prediction) -- The prediction to add.
+        """
+        self.predictions.append(prediction)
+
+    def get_accuracy(self) -> float:
+        """
+        Returns the accuracy of this Predictoor, defined as the proportion of correct predictions out of all predictions made.
+        @return
+            accuracy (float) -- The accuracy of this Predictoor.
+        """
+        n_predictions = len(self.predictions)
+        if n_predictions < MIN_PREDICTIONS:
+            return 0
+        n_correct = sum(
+            1 for prediction in self.predictions if prediction.correct)
+        return n_correct / n_predictions
