c0cf383896b4d4b56dac20dcce260e5a7f812b28
trizin <25263018+trizin@users.noreply.github.com>
2023-05-26 16:11:36 +0300

    Use pytest parametrize

--- util/test/predictoor/test_models.py
+++ util/test/predictoor/test_models.py
@@ -35,33 +35,32 @@ def test_prediction_from_query_result():
         Prediction.from_query_result(prediction_dict)
 
 
-def test_predictoor_get_accuracy():
-    # accuracy is 0 if there are no predictions
+@pytest.mark.parametrize(
+    "predictions, expected_accuracy",
+    [
+        ([], 0),
+        ([Prediction(5, 0.5, "0x123")], 1),
+        (
+            [
+                Prediction(5, 0.0, "0x123"),
+                Prediction(5, 0.5, "0x123"),
+                Prediction(5, 0.5, "0x123"),
+            ],
+            2 / 3,
+        ),
+        ([Prediction(2, 1.0, "0x123") for i in range(MIN_PREDICTIONS)], 1),
+        ([Prediction(2, 0.0, "0x123") for i in range(MIN_PREDICTIONS)], 0),
+        (
+            [
+                Prediction(2, 1.0 if i % 2 == 0 else 0.0, "0x123")
+                for i in range(MIN_PREDICTIONS)
+            ],
+            0.5,
+        ),
+    ],
+)
+def test_predictor_accuracy(predictions, expected_accuracy):
     predictoor = Predictoor("0x123")
-    assert predictoor.get_accuracy() == 0
-
-    predictoor.add_prediction(Prediction(5, 0.5, "0x123"))
-    assert predictoor.get_accuracy() == 1
-
-    # accuracy is correctly calculated
-    # all predictions correct
-    predictoor = Predictoor("0x123")
-    for i in range(0, MIN_PREDICTIONS):
-        predictoor.add_prediction(Prediction(2, 1.0, "0x123"))
-    assert predictoor.get_accuracy() == 1
-
-    # all predictions wrong
-    predictoor = Predictoor("0x123")
-    for i in range(0, MIN_PREDICTIONS):
-        predictoor.add_prediction(Prediction(2, 0.0, "0x123"))
-    assert predictoor.get_accuracy() == 0
-
-    # half of the predictions are correct
-    predictoor = Predictoor("0x123")
-    n_predictions = MIN_PREDICTIONS + MIN_PREDICTIONS % 2
-    for i in range(0, n_predictions):
-        if i % 2 == 0:
-            predictoor.add_prediction(Prediction(2, 1.0, "0x123"))
-        else:
-            predictoor.add_prediction(Prediction(2, 0.0, "0x123"))
-    assert predictoor.get_accuracy() == 0.5
+    for prediction in predictions:
+        predictoor.add_prediction(prediction)
+    assert predictoor.get_accuracy() == expected_accuracy
