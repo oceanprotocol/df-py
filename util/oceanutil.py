ebd7e04aeee1057ccc66e596da3a2065a2c1b795
trizin <25263018+trizin@users.noreply.github.com>
2023-05-31 21:54:43 +0300

    Remove staking contract

--- util/oceanutil.py
+++ util/oceanutil.py
@@ -56,7 +56,6 @@ def recordDeployedContracts(address_file: str):
     C["ERC721Template"] = B.ERC721Template.at(a["ERC721Template"]["1"])
     C["ERC20Template"] = B.ERC20Template.at(a["ERC20Template"]["1"])
     C["Router"] = B.FactoryRouter.at(a["Router"])
-    C["Staking"] = B.SideStaking.at(a["Staking"])
     C["ERC721Factory"] = B.ERC721Factory.at(a["ERC721Factory"])
     C["FixedPrice"] = B.FixedRateExchange.at(a["FixedPrice"])
 
@@ -98,10 +97,6 @@ def ERC20Template():
 
 def factoryRouter():
     return _contracts("Router")
-
-
-def Staking():
-    return _contracts("Staking")
 
 
 def ERC721Factory():
