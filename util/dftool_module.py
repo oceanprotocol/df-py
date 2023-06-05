1dbd8540efc7bd9a18b4855713620fe65abc2e57
trizin <25263018+trizin@users.noreply.github.com>
2023-06-05 16:14:15 +0300

    Directly import savePredictoorData function

--- util/dftool_module.py
+++ util/dftool_module.py
@@ -19,7 +19,7 @@ from util import (
     networkutil,
     query,
 )
-from util.predictoor import csvs as predictoor_csvs
+from util.predictoor.csvs import savePredictoorData
 from util.predictoor.query import queryPredictoors
 from util.base18 import from_wei
 from util.blocktime import getfinBlock, timestrToTimestamp, getstfinBlocks
@@ -517,7 +517,7 @@ Usage: dftool predictoor_data CSV_DIR START_DATE END_DATE CHAINID [RETRIES]
         fin_block,
         CHAINID,
     )
-    predictoor_csvs.savePredictoorData(predictoor_data, CSV_DIR, CHAINID)
+    savePredictoorData(predictoor_data, CSV_DIR, CHAINID)
     print("dftool predictoor_data: Done")
 
 
