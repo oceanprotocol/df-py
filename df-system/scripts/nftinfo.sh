00d59fd9909df0e0a0542efd6e45b0719ad99ee6
trizin <25263018+trizin@users.noreply.github.com>
2023-06-05 19:19:40 +0300

    Update nftinfo server script

--- df-system/scripts/nftinfo.sh
+++ df-system/scripts/nftinfo.sh
@@ -1,7 +1,4 @@
 mkdir -p /tmp/dfpy
 dfpy_docker nftinfo /app/data 1
-dfpy_docker nftinfo /app/data 56
 dfpy_docker nftinfo /app/data 137
-dfpy_docker nftinfo /app/data 246
-dfpy_docker nftinfo /app/data 1285
 mv /tmp/dfpy/nftinfo* ~/.dfcsv
