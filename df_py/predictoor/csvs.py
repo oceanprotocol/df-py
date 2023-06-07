d055e9a1c5b9c0442ec15f71ee0c14fba56b9ae2
trizin <25263018+trizin@users.noreply.github.com>
2023-06-07 14:51:58 +0300

    Predictoor rewards csv functions

--- df_py/predictoor/csvs.py
+++ df_py/predictoor/csvs.py
@@ -74,3 +74,52 @@ def loadPredictoorData(csv_dir: str, chainid: int) -> Dict[str, PredictoorBase]:
 def predictoorDataFilename(csv_dir, chainid):
     f = f"predictoordata_{chainid}.csv"
     return os.path.join(csv_dir, f)
+
+# ------------------------------- REWARDS -------------------------------
+
+
+@enforce_types
+def savePredictoorRewards(
+    predictoor_rewards: Dict[str, float],
+    csv_dir: str,
+):
+    assert os.path.exists(csv_dir), csv_dir
+    csv_file = predictoorRewardsFilename(csv_dir)
+    assert not os.path.exists(csv_file), csv_file
+
+    with open(csv_file, "w") as f:
+        writer = csv.writer(f)
+        row = ["predictoor_addr", "reward"]
+        writer.writerow(row)
+        for predictoor_addr, reward in predictoor_rewards.items():
+            assertIsEthAddr(predictoor_addr)
+            row = [predictoor_addr.lower(), str(reward)]
+            writer.writerow(row)
+    print(f"Created {csv_file}")
+
+
+@enforce_types
+def loadPredictoorRewards(csv_dir: str) -> Dict[str, float]:
+    csv_file = predictoorRewardsFilename(csv_dir)
+    predictoor_rewards = {}
+    with open(csv_file, "r") as f:
+        reader = csv.reader(f)
+        for row_i, row in enumerate(reader):
+            if row_i == 0:
+                assert row == ["predictoor_addr", "reward"]
+                continue
+            predictoor_addr, reward_s = row
+            predictoor_addr = predictoor_addr.lower()
+            reward = float(reward_s)
+            assertIsEthAddr(predictoor_addr)
+
+            predictoor_rewards[predictoor_addr] = reward
+
+    print(f"Loaded {csv_file}")
+    return predictoor_rewards
+
+
+@enforce_types
+def predictoorRewardsFilename(csv_dir):
+    f = f"predictoor_rewards.csv"
+    return os.path.join(csv_dir, f)
