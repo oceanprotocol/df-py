7737dd72267b40840f0ea6a00a0547ef0aeb3f13
trizin <25263018+trizin@users.noreply.github.com>
2023-06-07 15:00:07 +0300

    Add  test

--- df_py/predictoor/test/test_predictoor_csvs.py
+++ df_py/predictoor/test/test_predictoor_csvs.py
@@ -47,3 +47,33 @@ def test_predictoordata(tmp_path):
             loaded_predictoor.correct_prediction_count
             == original_predictoor.correct_prediction_count
         )
+
+
+@enforce_types
+def test_predictoorrewards(tmp_path):
+    target_csv = """predictoor_addr,reward
+0x0000000000000000000000000000000000000000,10.0
+0x1000000000000000000000000000000000000000,20.0
+0x2000000000000000000000000000000000000000,30.0
+0x3000000000000000000000000000000000000000,40.0
+0x4000000000000000000000000000000000000000,50.0"""
+
+    # generate random rewards
+    predictoor_rewards = {}
+    for i in range(5):
+        predictoor_rewards[f"0x{i}000000000000000000000000000000000000000"] = (i + 1) * 10.0
+
+    csv_dir = str(tmp_path)
+    csvs.savePredictoorRewards(predictoor_rewards, csv_dir)
+
+    with open(csvs.predictoorRewardsFilename(csv_dir), "r") as loaded_data:
+        data = loaded_data.read().strip()
+        assert data == target_csv
+
+    loaded_predictoor_rewards = csvs.loadPredictoorRewards(csv_dir)
+    assert len(loaded_predictoor_rewards) == len(predictoor_rewards)
+
+    # loaded rewards should be equal to originally created ones
+    for addr, original_reward in predictoor_rewards.items():
+        loaded_reward = loaded_predictoor_rewards[addr]
+        assert loaded_reward == original_reward
