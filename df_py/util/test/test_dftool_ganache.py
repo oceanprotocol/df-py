5d002dd3567429a29ffea725eb7468e0098624d2
trizin <25263018+trizin@users.noreply.github.com>
2023-06-09 11:39:01 +0000

    Update tests

--- df_py/util/test/test_dftool_ganache.py
+++ df_py/util/test/test_dftool_ganache.py
@@ -54,8 +54,7 @@ def test_calc(tmp_path):
     # main cmd
     TOT_OCEAN = 1000.0
     START_DATE = "2023-02-02"  # Only substream is volume DF
-    SUBSTREAM_NAME = "volume"
-    cmd = f"./dftool calc {CSV_DIR} {TOT_OCEAN} {START_DATE} {SUBSTREAM_NAME}"
+    cmd = f"./dftool calc_volume_rewards {CSV_DIR} {TOT_OCEAN} {START_DATE}"
     os.system(cmd)
 
     # test result
@@ -115,7 +114,7 @@ def test_calc_predictoor_substream(tmp_path):
     # TEST WITH TOT_OCEAN > 0
     TOT_OCEAN = 1000.0
     ST = "2023-03-16"  # first week of df main
-    cmd = f"./dftool calc {CSV_DIR} {TOT_OCEAN} {ST} {SUBSTREAM}"
+    cmd = f"./dftool calc_predictoor_rewards {CSV_DIR} {TOT_OCEAN} {ST}"
     os.system(cmd)
 
     # test result
@@ -133,7 +132,7 @@ def test_calc_predictoor_substream(tmp_path):
     # TEST WITH TOT_OCEAN = 0, DATE WITH NONZERO REWARDS
     TOT_OCEAN = 0
     ST = "2025-03-16"  # some date where predictoor rewards are nonzero
-    cmd = f"./dftool calc {CSV_DIR} {TOT_OCEAN} {ST} {SUBSTREAM}"
+    cmd = f"./dftool calc_predictoor_rewards {CSV_DIR} {TOT_OCEAN} {ST}"
     os.system(cmd)
 
     # test result
@@ -149,7 +148,7 @@ def test_calc_predictoor_substream(tmp_path):
     # TEST WITH TOT_OCEAN = 0, DATE WITH ZERO REWARDS
     TOT_OCEAN = 0
     ST = "2023-01-01"  # some date where predictoor rewards are zero
-    cmd = f"./dftool calc {CSV_DIR} {TOT_OCEAN} {ST} {SUBSTREAM}"
+    cmd = f"./dftool calc_predictoor_rewards {CSV_DIR} {TOT_OCEAN} {ST}"
     os.system(cmd)
 
     # test result
@@ -188,7 +187,7 @@ def test_calc_without_amount(tmp_path):
     # main cmd
     TOT_OCEAN = 0
     ST = "2023-03-16"  # first week of df main
-    cmd = f"./dftool calc {CSV_DIR} {TOT_OCEAN} {ST}"
+    cmd = f"./dftool calc_volume_rewards {CSV_DIR} {TOT_OCEAN} {ST}"
     os.system(cmd)
 
     # test result
