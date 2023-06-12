0764fcf125414c977700148287d0e4e3d14e7cff
Calina Cenan <calina@cenan.net>
2023-06-12 13:38:36 +0000

    Add some coverage for dftool arguments.

---
+++ df_py/util/test/test_dftool_arguments.py
@@ -0,0 +1,57 @@
+import argparse
+import pytest
+
+from datetime import datetime
+from df_py.util.dftool_arguments import (
+    DfStrategyArgumentParser,
+    SimpleChainIdArgumentParser,
+    StartFinArgumentParser,
+    autocreate_path,
+    block_or_valid_date,
+    challenge_date,
+    do_help_long,
+    do_help_short,
+    existing_path,
+    print_arguments,
+    valid_date,
+    valid_date_and_convert,
+)
+
+
+def test_valid_date_and_convert():
+    assert isinstance(valid_date_and_convert("2020-01-01"), datetime)
+
+    with pytest.raises(argparse.ArgumentTypeError, match="not a valid date"):
+        valid_date_and_convert("a string")
+
+
+def test_valid_date():
+    assert valid_date("2020-01-01") == "2020-01-01"
+
+    with pytest.raises(argparse.ArgumentTypeError, match="not a valid date"):
+        valid_date("a string")
+
+
+def test_block_or_valid_date():
+    assert block_or_valid_date("latest") == "latest"
+    assert block_or_valid_date("14") == 14
+    assert block_or_valid_date("2020-01-01") == "2020-01-01"
+    assert block_or_valid_date("2020-01-01_14:30") == "2020-01-01_14:30"
+
+    with pytest.raises(argparse.ArgumentTypeError, match="not a valid date"):
+        block_or_valid_date("a string")
+
+
+def test_autocreate_path(tmp_path):
+    path = tmp_path / "test"
+    assert autocreate_path(path) == path
+    assert path.exists()
+
+
+def test_existing_path(tmp_path):
+    path = tmp_path / "test"
+    path.mkdir()
+    assert existing_path(path) == path
+
+    with pytest.raises(argparse.ArgumentTypeError, match="doesn't exist"):
+        existing_path(tmp_path / "not_existing")
