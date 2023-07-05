afb3f514a3395c8d4dc2283df43af6d060fe3bae
trizin <25263018+trizin@users.noreply.github.com>
2023-07-05 22:36:53 +0000

    Add import

--- df_py/predictoor/test/test_predictoor_calc_rewards.py
+++ df_py/predictoor/test/test_predictoor_calc_rewards.py
@@ -5,7 +5,7 @@ from unittest.mock import patch
 import pytest
 
 from df_py.predictoor.calc_rewards import calc_predictoor_rewards, filter_predictoors
-from df_py.predictoor.models import Predictoor, PredictoorBase
+from df_py.predictoor.models import Predictoor, PredictoorBase, Prediction
 from df_py.util.constants import MIN_PREDICTIONS
 from df_py.util.networkutil import DEV_CHAINID
 
