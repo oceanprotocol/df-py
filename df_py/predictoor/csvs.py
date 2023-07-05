5103dcd59dd281e4a56d9a3f601005d799b20d66
trizin <25263018+trizin@users.noreply.github.com>
2023-07-05 11:04:00 +0000

    Update csv structure

--- df_py/predictoor/csvs.py
+++ df_py/predictoor/csvs.py
@@ -11,66 +11,52 @@ from df_py.util.csv_helpers import assert_is_eth_addr
 # ------------------------------- PREDICTOOR DATA -------------------------------
 @enforce_types
 def save_predictoor_data_csv(
-    predictoor_data: Dict[str, Union[PredictoorBase, Predictoor]],
+    predictoor_data: Dict[str, Predictoor],
     csv_dir: str,
 ):
     assert os.path.exists(csv_dir), csv_dir
     csv_file = predictoor_data_csv_filename(csv_dir)
     assert not os.path.exists(csv_file), csv_file
 
-    with open(csv_file, "w") as f:
-        writer = csv.writer(f)
-        row = ["predictoor_addr", "accuracy", "n_preds", "n_correct_preds"]
-        writer.writerow(row)
-
-        for predictoor in predictoor_data.values():
-            accuracy = predictoor.accuracy
-            predictoor_addr = predictoor.address
-            n_preds = predictoor.prediction_count
-            n_correct_preds = predictoor.correct_prediction_count
-            assert_is_eth_addr(predictoor_addr)
-            row = [
-                predictoor_addr.lower(),
-                str(accuracy),
-                str(n_preds),
-                str(n_correct_preds),
-            ]
-            writer.writerow(row)
+    fieldnames = ['address', 'slot', 'payout', 'contract_addr']
+    with open(csv_file, mode='w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        for predictoor in predictoors:
+            address = predictoor.address
+            for prediction in predictoor._predictions:
+                writer.writerow({
+                    'address': address,
+                    'slot': prediction.slot,
+                    'payout': prediction.payout,
+                    'contract_addr': prediction.contract_addr
+                })
 
     print(f"Created {csv_file}")
 
 
+
 @enforce_types
-def load_predictoor_data_csv(csv_dir: str) -> Dict[str, PredictoorBase]:
+def load_predictoor_data_csv(csv_dir: str) -> Dict[str, Predictoor]:
     csv_file = predictoor_data_csv_filename(csv_dir)
-    predictoor_data = {}
-    with open(csv_file, "r") as f:
-        reader = csv.reader(f)
+    
+    predictoors = {}
+    with open(file_path, mode='r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            address = row['address']
+            slot = int(row['slot'])
+            payout = float(row['payout'])
+            contract_addr = row['contract_addr']
+            prediction = Prediction(slot, payout, contract_addr)
 
-        for row_i, row in enumerate(reader):
-            if row_i == 0:
-                assert row == [
-                    "predictoor_addr",
-                    "accuracy",
-                    "n_preds",
-                    "n_correct_preds",
-                ]
-                continue
-            predictoor_addr, accuracy_s, n_preds_s, n_correct_preds_s = row
-            predictoor_addr = predictoor_addr.lower()
-            accuracy = float(accuracy_s)
-            n_preds = int(n_preds_s)
-            n_correct_preds = int(n_correct_preds_s)
-            assert_is_eth_addr(predictoor_addr)
-
-            predictoor = PredictoorBase(
-                predictoor_addr, n_preds, n_correct_preds, accuracy
-            )
-
-            predictoor_data[predictoor_addr] = predictoor
-
+            if address not in predictoors:
+                predictoors[address] = Predictoor(address)
+                
+            predictoors[address].add_prediction(prediction)
+                
     print(f"Loaded {csv_file}")
-    return predictoor_data
+    return predictoors
 
 
 @enforce_types
