fbc267e3a94e4261bec0c641b088cde9ccf08f95
Calina Cenan <calina@cenan.net>
2023-10-11 08:51:24 +0000

    Fix Simpletoken tests TODOs.

--- df_py/util/test/test_Simpletoken.py
+++ df_py/util/test/test_Simpletoken.py
@@ -8,11 +8,11 @@ from df_py.util.contract_base import ContractBase
 def test_transfer(w3, all_accounts):
     accounts = all_accounts
     token = _deploy_token(w3)
+    initial_balance = token.balanceOf(accounts[0])
     assert token.totalSupply() == to_wei(1e21)
     token.transfer(accounts[1], to_wei(1e20), {"from": accounts[0]})
     assert token.balanceOf(accounts[1]) == to_wei(1e20)
-    # TODO
-    # assert token.balanceOf(accounts[0]) == to_wei(9e20)
+    assert token.balanceOf(accounts[0]) == initial_balance - to_wei(1e20)
 
 
 @enforce_types
@@ -31,14 +31,15 @@ def test_approve(w3, all_accounts):
 def test_transferFrom(w3, all_accounts):
     accounts = all_accounts
     token = _deploy_token(w3)
+    initial_balance = token.balanceOf(accounts[0])
+
     token.approve(accounts[1], to_wei(6e18), {"from": accounts[0]})
     token.transferFrom(accounts[0], accounts[2], to_wei(5e18), {"from": accounts[1]})
 
     assert token.balanceOf(accounts[2]) == to_wei(5e18)
     assert token.balanceOf(accounts[1]) == 0
-    # TODO
-    # assert token.balanceOf(accounts[0]) == to_wei(9.95e20)
-    # assert token.allowance(accounts[0], accounts[1]) == to_wei(1e18)
+
+    assert token.balanceOf(accounts[0]) == initial_balance - to_wei(5e18)
 
 
 @enforce_types
