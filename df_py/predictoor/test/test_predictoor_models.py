1f941cffe792d5901ef0d5b4e29d41958be8e673
trizin <25263018+trizin@users.noreply.github.com>
2023-11-02 12:21:35 +0000

    Fix type

--- df_py/predictoor/test/test_predictoor_models.py
+++ df_py/predictoor/test/test_predictoor_models.py
@@ -4,23 +4,23 @@ from df_py.predictoor.models import PredictContract, Prediction, Predictoor
 
 
 def test_prediction_init():
-    prediction = Prediction(123, 1.23, 1, "0x1")
+    prediction = Prediction(123, 1.23, 1.0, "0x1")
     assert prediction.slot == 123
     assert prediction.payout == 1.23
     assert prediction.contract_addr == "0x1"
 
 
 def test_prediction_is_correct():
-    prediction = Prediction(123, 1.23, 1, "0x1")
+    prediction = Prediction(123, 1.23, 1.0, "0x1")
     assert prediction.is_correct
-    prediction = Prediction(123, 0.0, 1, "0x1")
+    prediction = Prediction(123, 0.0, 1.0, "0x1")
     assert not prediction.is_correct
 
 def test_prediction_profit():
-    prediction = Prediction(123, 10, 1, "0x1")
+    prediction = Prediction(123, 10, 1.0, "0x1")
     assert prediction.profit == 10
 
-    prediction = Prediction(123, 0, 1, "0x1")
+    prediction = Prediction(123, 0, 1.0, "0x1")
     assert prediction.profit == -1
 
 
@@ -48,19 +48,19 @@ def test_prediction_from_query_result():
     "predictions, expected_accuracy",
     [
         ([], 0),
-        ([Prediction(5, 0.5, 1, "0x123")], 1),
+        ([Prediction(5, 0.5, 1.0, "0x123")], 1),
         (
             [
-                Prediction(5, 0.0, 1, "0x123"),
-                Prediction(5, 0.5, 1, "0x123"),
-                Prediction(5, 0.5, 1, "0x123"),
+                Prediction(5, 0.0, 1.0, "0x123"),
+                Prediction(5, 0.5, 1.0, "0x123"),
+                Prediction(5, 0.5, 1.0, "0x123"),
             ],
             2 / 3,
         ),
-        ([Prediction(2, 1.0, 1, "0x123") for _ in range(100)], 1),
-        ([Prediction(2, 0.0, 1, "0x123") for _ in range(100)], 0),
+        ([Prediction(2, 1.0, 1.0, "0x123") for _ in range(100)], 1),
+        ([Prediction(2, 0.0, 1.0, "0x123") for _ in range(100)], 0),
         (
-            [Prediction(2, 1.0 if i % 2 == 0 else 0.0, 1, "0x123") for i in range(100)],
+            [Prediction(2, 1.0 if i % 2 == 0 else 0.0, 1.0, "0x123") for i in range(100)],
             0.5,
         ),
     ],
