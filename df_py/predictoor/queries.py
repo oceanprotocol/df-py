b93219c42b7a751a44ddc3be394baa48f6fb5c2c
trizin <25263018+trizin@users.noreply.github.com>
2023-11-09 09:54:35 +0000

    Calculate and include timeframe and source in asset name

--- df_py/predictoor/queries.py
+++ df_py/predictoor/queries.py
@@ -1,11 +1,50 @@
-from typing import Dict
+from typing import Dict, Optional
 
 from enforce_typing import enforce_types
+from web3 import Web3
 
 from df_py.predictoor.models import PredictContract, Prediction, Predictoor
 from df_py.util.constants import DEPLOYER_ADDRS
 from df_py.util.graphutil import submit_query
 from df_py.util.networkutil import DEV_CHAINID
+
+
+@enforce_types
+def key_to_725(key: str):
+    key725 = Web3.keccak(key.encode("utf-8")).hex()
+    return key725
+
+
+@enforce_types
+def value_from_725(value725) -> str:
+    value = Web3.to_text(hexstr=value725)
+    return value
+
+
+@enforce_types
+def info_from_725(info725_list: list) -> Dict[str, Optional[str]]:
+    """
+    @arguments
+      info725_list -- eg [{"key":encoded("pair"), "value":encoded("ETH/USDT")},
+                          {"key":encoded("timeframe"), "value":encoded("5m") },
+                           ... ]
+    @return
+      info_dict -- e.g. {"pair": "ETH/USDT",
+                         "timeframe": "5m",
+                          ... }
+    """
+    target_keys = ["pair", "timeframe", "source", "base", "quote"]
+    info_dict: Dict[str, Optional[str]] = {}
+    for key in target_keys:
+        info_dict[key] = None
+        for item725 in info725_list:
+            key725, value725 = item725["key"], item725["value"]
+            if key725 == key_to_725(key):
+                value = value_from_725(value725)
+                info_dict[key] = value
+                break
+
+    return info_dict
 
 
 @enforce_types
@@ -44,6 +83,10 @@ def query_predictoor_contracts(chain_id: int) -> Dict[str, PredictContract]:
                         owner {
                             id
                         }
+                        nftData {
+                            key
+                            value
+                        }
                     }
                 }
                 secondsPerEpoch
@@ -70,10 +113,18 @@ def query_predictoor_contracts(chain_id: int) -> Dict[str, PredictContract]:
                 continue
 
             nft_addr = contract["token"]["nft"]["id"]
+            info725 = contract["token"]["nft"]["nftData"]
+            info = info_from_725(info725)
+            pair = info["pair"]
+            timeframe = info["timeframe"]
+            source = info["source"]
+
+            asset_name = f"{pair}-{timeframe}-{source}"
+
             contract_obj = PredictContract(
                 chain_id,
                 nft_addr,
-                contract["token"]["name"],
+                asset_name,
                 contract["token"]["symbol"],
                 contract["secondsPerEpoch"],
                 contract["secondsPerSubscription"],
