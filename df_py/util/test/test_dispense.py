db2f7a8effff30bd489488bb7dcbbb1c81d7f907
trizin <25263018+trizin@users.noreply.github.com>
2023-11-21 09:26:56 +0000

    Add a test for multisig transfer tokens

--- df_py/util/test/test_dispense.py
+++ df_py/util/test/test_dispense.py
@@ -108,6 +108,14 @@ def test_dispense_passive(w3):
     assert mock.call_count == 3
 
 
+def test_multisig_transfer_tokens(w3):
+    OCEAN = oceanutil.OCEAN_token(networkutil.DEV_CHAINID)
+    with patch("df_py.util.dispense.chain_id_to_multisig_addr"):
+        with patch("df_py.util.dispense.send_multisig_tx") as mock:
+            dispense.dispense_passive(w3, OCEAN, OCEAN.address, 1)
+    assert mock.call_count == 1
+
+
 @enforce_types
 def setup_function():
     oceantestutil.fill_accounts_with_OCEAN(accounts)
