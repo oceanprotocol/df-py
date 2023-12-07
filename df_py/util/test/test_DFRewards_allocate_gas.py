01216184b12a119a1200a0ca682bd0b1222a74d1
Calina Cenan <calina@cenan.net>
2023-12-07 18:27:54 +0000

    Remove df rewards allocate gas test.

--- df_py/util/test/test_DFRewards_allocate_gas.py
+++ df_py/util/test/test_DFRewards_allocate_gas.py
@@ -8,26 +8,6 @@ from df_py.util.contract_base import ContractBase
 from df_py.util.random_addresses import get_random_addresses
 
 
-@enforce_types
-def test_allocate_gas(w3, account0):
-    one = _batch_allocate(w3, account0, 6)
-    two = _batch_allocate(w3, account0, 9)
-    nine = _batch_allocate(w3, account0, 9)
-    ten = _batch_allocate(w3, account0, 10)
-
-    per_iteration1 = two.gasUsed - one.gasUsed
-    per_iteration2 = ten.gasUsed - nine.gasUsed
-
-    # each iteration uses the same amount of gas
-    assert abs(per_iteration2 - per_iteration1) < 150
-
-    # 23167 is the estimated gas for each iteration
-    assert abs(per_iteration1 - 23167) < 150
-
-    # mainnet gas limit
-    assert per_iteration1 * 1250 < 30_000_000
-
-
 # TODO: fix this test pylint: disable=fixme
 @pytest.mark.skip("Skip after fixing and removing brownie")
 @enforce_types
