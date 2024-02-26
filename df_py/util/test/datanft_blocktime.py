80c06a1c985d8c3bb5954611573f6ee4a9c33407
trizin <25263018+trizin@users.noreply.github.com>
2024-02-26 15:52:56 +0300

    Add read_data function

--- df_py/util/test/datanft_blocktime.py
+++ df_py/util/test/datanft_blocktime.py
@@ -14,3 +14,15 @@ def _set_data(w3, nft_addr: str, field_label: str, data: str) -> bool:
     ).transact()
     receipt = self.config.w3.eth.wait_for_transaction_receipt(tx)
     return receipt is not None and receipt["status"] == 1
+
+
+def _read_data(w3, nft_addr: str, field_label: str) -> str:
+    field_label_hash = Web3.keccak(text=field_label)
+    field_value_bytes = field_value.encode() 
+    contract_instance = ContractBase(w3, "ERC721Template", constructor_args=[])
+    value = contract_instance.functions.getData(
+        field_label_hash
+    ).call()
+
+    value_str = value.decode("utf-8")
+    return value_str
