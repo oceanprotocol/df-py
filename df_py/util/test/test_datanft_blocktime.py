c4a759e72536e0c473e3d561304974c149749687
trizin <25263018+trizin@users.noreply.github.com>
2024-02-27 12:50:06 +0000

    Add  test

--- df_py/util/test/test_datanft_blocktime.py
+++ df_py/util/test/test_datanft_blocktime.py
@@ -28,3 +28,22 @@ def test_datanft_write_and_read(w3, account0):
     # Read the block number again, should return 100
     block_number = get_block_number_from_datanft(CHAIN_ID, w3)
     assert block_number == 100, "block number must be 100"
+
+def test_multiple_chainids_write_and_read(w3, account0):
+    chain_ids_and_block_numbers = {
+        1: 1000,
+        2: 2000,
+        3: 3000,
+    }
+
+    for chain_id, block_number in chain_ids_and_block_numbers.items():
+        assert set_blocknumber_to_datanft(
+            chain_id, account0.address, block_number, w3
+        ), f"Failed to set block number for chain ID {chain_id}"
+
+    for chain_id, expected_block_number in chain_ids_and_block_numbers.items():
+        actual_block_number = get_block_number_from_datanft(chain_id, w3)
+        assert (
+            actual_block_number == expected_block_number
+        ), f"Block number for chain ID {chain_id} must be {expected_block_number}"
+
